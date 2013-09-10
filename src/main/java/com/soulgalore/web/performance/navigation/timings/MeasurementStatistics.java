a302d9314d95e3e53990767f59a563b6fed3bf7d
Tobias Lidskog <tobias@lidskog.se>
2013-09-11 01:09:12 +0200

    Include statistics in xml output.

--- src/main/java/com/soulgalore/web/performance/navigation/timings/MeasurementStatistics.java
+++ src/main/java/com/soulgalore/web/performance/navigation/timings/MeasurementStatistics.java
@@ -2,12 +2,12 @@ package com.soulgalore.web.performance.navigation.timings;
 
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 
+import javax.xml.bind.annotation.XmlElement;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-/**
- *
- */
 public class MeasurementStatistics {
     private final Map<String, DescriptiveStatistics> statistics = new HashMap<String, DescriptiveStatistics>();
 
@@ -24,4 +24,52 @@ public class MeasurementStatistics {
         }
         return stats;
     }
+
+    @XmlElement(name = "statistic")
+    public List<Statistic> getStatistics() {
+        List<Statistic> result = new ArrayList<Statistic>(statistics.size());
+
+        for (Map.Entry<String, DescriptiveStatistics> entry : statistics.entrySet()) {
+            result.add(Statistic.create(entry.getKey(), entry.getValue()));
+        }
+        
+        return result;
+    }
+
+    public static class Statistic {
+        @XmlElement
+        String name;
+        @XmlElement
+        double min;
+        @XmlElement
+        double avg;
+        @XmlElement
+        double median;
+        @XmlElement
+        double p60;
+        @XmlElement
+        double p70;
+        @XmlElement
+        double p80;
+        @XmlElement
+        double p90;
+        @XmlElement
+        double max;
+
+        public static Statistic create(String name, DescriptiveStatistics statistics) {
+            Statistic statistic = new Statistic();
+            statistic.name = name;
+            statistic.min = statistics.getMin();
+            statistic.avg = statistics.getMean();
+            statistic.median = statistics.getPercentile(50);
+            statistic.p60 = statistics.getPercentile(60);
+            statistic.p70 = statistics.getPercentile(70);
+            statistic.p80 = statistics.getPercentile(80);
+            statistic.p90 = statistics.getPercentile(90);
+            statistic.max = statistics.getMax();
+
+            return statistic;
+        }
+    }
+
 }
