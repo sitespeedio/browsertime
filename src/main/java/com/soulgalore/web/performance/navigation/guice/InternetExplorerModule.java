596f62e10ca28aba6bc7723868e8ba14c8009f75
Peter Hedenskog <peter@soulgalore.com>
2013-09-11 08:07:48 +0200

    added/updated license & added Tobias :)

--- src/main/java/com/soulgalore/web/performance/navigation/guice/InternetExplorerModule.java
+++ src/main/java/com/soulgalore/web/performance/navigation/guice/InternetExplorerModule.java
@@ -1,10 +1,10 @@
-/******************************************************
- * Navigation timing
+ /*******************************************************************************************************************************
+ * It's Browser Time!
  * 
  *
- * Copyright (C) 2013 by Peter Hedenskog (http://peterhedenskog.com)
+ * Copyright (C) 2013 by Tobias Lidskog (https://twitter.com/tobiaslidskog) &  Peter Hedenskog (http://peterhedenskog.com)
  *
- ******************************************************
+ ********************************************************************************************************************************
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in 
  * compliance with the License. You may obtain a copy of the License at
@@ -16,7 +16,7 @@
  * distributed  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   
  * See the License for the specific language governing permissions and limitations under the License.
  *
- *******************************************************
+ ********************************************************************************************************************************
  */
 package com.soulgalore.web.performance.navigation.guice;
 
