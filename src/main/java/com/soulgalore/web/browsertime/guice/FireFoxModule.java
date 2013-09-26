2bdfbf1f6ff74910d94f8d38fad65181b92807f9
Tobias Lidskog <tobias@lidskog.se>
2013-09-26 22:57:40 +0200

    Disable cacheing in Firefox by using a custom profile.

--- src/main/java/com/soulgalore/web/browsertime/guice/FireFoxModule.java
+++ src/main/java/com/soulgalore/web/browsertime/guice/FireFoxModule.java
@@ -1,6 +1,6 @@
- /*******************************************************************************************************************************
+/*******************************************************************************************************************************
  * It's Browser Time!
- * 
+ *
  *
  * Copyright (C) 2013 by Tobias Lidskog (https://twitter.com/tobiaslidskog) &  Peter Hedenskog (http://peterhedenskog.com)
  *
@@ -8,7 +8,7 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in 
  * compliance with the License. You may obtain a copy of the License at
- * 
+ *
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -21,20 +21,38 @@
 package com.soulgalore.web.browsertime.guice;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Provider;
 import com.soulgalore.web.browsertime.SeleniumTimingRunner;
 import com.soulgalore.web.browsertime.TimingRunner;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
 
 /**
  * Setup a module that uses Firefox.
  */
 public class FireFoxModule extends AbstractModule {
 
-	@Override
-	protected void configure() {
-		bind(WebDriver.class).to(FirefoxDriver.class);
+    @Override
+    protected void configure() {
+        bind(WebDriver.class).toProvider(DRIVER_PROVIDER);
         bind(TimingRunner.class).to(SeleniumTimingRunner.class);
     }
+
+    private static final Provider<WebDriver> DRIVER_PROVIDER = new Provider<WebDriver>() {
+        @Override
+        public WebDriver get() {
+            return new FirefoxDriver(createNonCachingProfile());
+        }
+
+        private FirefoxProfile createNonCachingProfile() {
+            FirefoxProfile profile = new FirefoxProfile();
+            profile.setPreference("browser.cache.disk.enable", false);
+            profile.setPreference("browser.cache.memory.enable", false);
+            profile.setPreference("browser.cache.offline.enable", false);
+            profile.setPreference("network.http.use-cache", false);
+
+            return profile;
+        }
+    };
 }
