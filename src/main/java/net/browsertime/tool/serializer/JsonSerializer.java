2373613cd182a2cc7f284e7fad77061657ef81d3
Tobias Lidskog <tobias@lidskog.se>
2013-12-20 08:38:29 +0100

    More DRYing.

--- src/main/java/net/browsertime/tool/serializer/JsonSerializer.java
+++ src/main/java/net/browsertime/tool/serializer/JsonSerializer.java
@@ -69,31 +69,32 @@ public class JsonSerializer implements Serializer {
         writer.close();
     }
 
-    private static class StatisticsAdapter extends TypeAdapter<Statistics> {
+     private static void print(JsonWriter out, String name, String value) throws IOException {
+         out.name(name);
+         out.value(value);
+     }
+
+     private static void print(JsonWriter out, String name, double value) throws IOException {
+         out.name(name);
+         out.value(doubleFormat.format(value));
+     }
+
+     private static class StatisticsAdapter extends TypeAdapter<Statistics> {
         @Override
         public void write(JsonWriter out, Statistics value) throws IOException {
             List<Statistics.Statistic> statistics = value.getStatistics();
             out.beginArray();
             for (Statistics.Statistic statistic : statistics) {
                 out.beginObject();
-                out.name("name");
-                out.value(statistic.name);
-                out.name("min");
-                out.value(doubleFormat.format(statistic.min));
-                out.name("avg");
-                out.value(doubleFormat.format(statistic.avg));
-                out.name("median");
-                out.value(doubleFormat.format(statistic.median));
-                out.name("p60");
-                out.value(doubleFormat.format(statistic.p60));
-                out.name("p70");
-                out.value(doubleFormat.format(statistic.p70));
-                out.name("p80");
-                out.value(doubleFormat.format(statistic.p80));
-                out.name("p90");
-                out.value(doubleFormat.format(statistic.p90));
-                out.name("max");
-                out.value(doubleFormat.format(statistic.max));
+                print(out, "name", statistic.name);
+                print(out, "min", statistic.min);
+                print(out, "avg", statistic.avg);
+                print(out, "median", statistic.median);
+                print(out, "p60", statistic.p60);
+                print(out, "p70", statistic.p70);
+                print(out, "p80", statistic.p80);
+                print(out, "p90", statistic.p90);
+                print(out, "max", statistic.max);
                 out.endObject();
             }
             out.endArray();
@@ -150,15 +151,6 @@ public class JsonSerializer implements Serializer {
             out.endArray();
         }
 
-        private void print(JsonWriter out, String name, String value) throws IOException {
-            out.name(name);
-            out.value(value);
-        }
-
-        private void print(JsonWriter out, String name, double value) throws IOException {
-            out.name(name);
-            out.value(doubleFormat.format(value));
-        }
 
         @Override
          public TimingRun read(JsonReader in) throws IOException {
