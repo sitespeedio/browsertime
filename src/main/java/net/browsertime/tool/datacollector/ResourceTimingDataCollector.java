1392855fed8f3efc79521b39464505d850d196b6
soulgalore <peter@soulgalore.com>
2014-01-03 13:35:35 +0100

    skipping resources for IE for the moment, since it breakes #32

--- src/main/java/net/browsertime/tool/datacollector/ResourceTimingDataCollector.java
+++ src/main/java/net/browsertime/tool/datacollector/ResourceTimingDataCollector.java
@@ -22,7 +22,9 @@ package net.browsertime.tool.datacollector;
 
 import net.browsertime.tool.timings.TimingResourceMeasurement;
 import net.browsertime.tool.timings.TimingRun;
+
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.remote.RemoteWebElement;
 
 import java.util.List;
 import java.util.Map;
@@ -31,65 +33,75 @@ import java.util.Map;
  * Marks defined in the w3c resource timing recommendation.
  * http://www.w3.org/TR/resource-timing/
  * <p/>
- * NOTE: The user timing spec uses a different resolution for time stamps (milliseconds with a decimal part)
+ * NOTE: The user timing spec uses a different resolution for time stamps
+ * (milliseconds with a decimal part)
  */
 public class ResourceTimingDataCollector extends TimingDataCollector {
-    private static final String LIST_RESOURCES =
-            "return window.performance.getEntriesByType('resource');";
+	private static final String LIST_RESOURCES = "return window.performance.getEntriesByType('resource');";
 
-    @Override
-    @SuppressWarnings("unchecked")
-    public void collectTimingData(JavascriptExecutor js, TimingRun results) {
-        if (!isPageDefinedTimingsSupported(js)) {
-            return;
-        }
+	@Override
+	@SuppressWarnings("unchecked")
+	public void collectTimingData(JavascriptExecutor js, TimingRun results) {
+		if (!isPageDefinedTimingsSupported(js)) {
+			return;
+		}
 
-        List<Map> resources = listFromJs(js, LIST_RESOURCES);
+		List<Map> resources = listFromJs(js, LIST_RESOURCES);
 
-        if (resources != null && !resources.isEmpty()) {
-            for (Map resource : resources) {
-                DoubleAdapter da = new DoubleAdapter(resource);
-                String name = (String) resource.get("name");
-                String initiatorType = (String) resource.get("initiatorType");
-                double startTime = da.asDouble("startTime");
-                double duration = da.asDouble("duration");
-                double redirectStart = da.asDouble("redirectStart");
-                double redirectEnd = da.asDouble("redirectEnd");
-                double fetchStart = da.asDouble("fetchStart");
-                double domainLookupStart = da.asDouble("domainLookupStart");
-                double domainLookupEnd = da.asDouble("domainLookupEnd");
-                double connectStart = da.asDouble("connectStart");
-                double connectEnd = da.asDouble("connectEnd");
-                double secureConnectionStart = da.asDouble("secureConnectionStart");
-                double requestStart = da.asDouble("requestStart");
-                double responseStart = da.asDouble("responseStart");
-                double responseEnd = da.asDouble("responseEnd");
-                results.addResourceMeasurement(new TimingResourceMeasurement(name, startTime, initiatorType,
-                        duration, redirectStart, redirectEnd,
-                        fetchStart, domainLookupStart, domainLookupEnd,
-                        connectStart, connectEnd, secureConnectionStart,
-                        requestStart, responseStart, responseEnd));
-            }
-        }
-    }
+		if (resources != null && !resources.isEmpty()) {
+			// When testing this on IE 11, I can't get hold of the resources
+			// as a Map, only the WebRemoteElement
+			if (resources.get(0) instanceof Map) {
+				for (Map resource : resources) {
+					DoubleAdapter da = new DoubleAdapter(resource);
+					String name = (String) resource.get("name");
+					String initiatorType = (String) resource
+							.get("initiatorType");
+					double startTime = da.asDouble("startTime");
+					double duration = da.asDouble("duration");
+					double redirectStart = da.asDouble("redirectStart");
+					double redirectEnd = da.asDouble("redirectEnd");
+					double fetchStart = da.asDouble("fetchStart");
+					double domainLookupStart = da.asDouble("domainLookupStart");
+					double domainLookupEnd = da.asDouble("domainLookupEnd");
+					double connectStart = da.asDouble("connectStart");
+					double connectEnd = da.asDouble("connectEnd");
+					double secureConnectionStart = da
+							.asDouble("secureConnectionStart");
+					double requestStart = da.asDouble("requestStart");
+					double responseStart = da.asDouble("responseStart");
+					double responseEnd = da.asDouble("responseEnd");
+					results.addResourceMeasurement(new TimingResourceMeasurement(
+							name, startTime, initiatorType, duration,
+							redirectStart, redirectEnd, fetchStart,
+							domainLookupStart, domainLookupEnd, connectStart,
+							connectEnd, secureConnectionStart, requestStart,
+							responseStart, responseEnd));
+				}
 
-    private boolean isPageDefinedTimingsSupported(JavascriptExecutor js) {
-        return booleanFromJs(js, "return !!(window.performance && window.performance.getEntriesByType);");
-    }
+			}
+		}
+	}
 
-    /**
-     * Helper class to simplify reading time stamps from browsers. Since 0 is interpreted by Selenium as an integer,
-     * it's represented as a Long. This class avoids ClassCastExceptions when reading all data as doubles.
-     */
-    private static class DoubleAdapter {
-        private final Map map;
+	private boolean isPageDefinedTimingsSupported(JavascriptExecutor js) {
+		return booleanFromJs(js,
+				"return !!(window.performance && window.performance.getEntriesByType);");
+	}
 
-        DoubleAdapter(Map map) {
-            this.map = map;
-        }
+	/**
+	 * Helper class to simplify reading time stamps from browsers. Since 0 is
+	 * interpreted by Selenium as an integer, it's represented as a Long. This
+	 * class avoids ClassCastExceptions when reading all data as doubles.
+	 */
+	private static class DoubleAdapter {
+		private final Map map;
 
-        double asDouble(String key) {
-            return Double.parseDouble(map.get(key).toString());
-        }
-    }
+		DoubleAdapter(Map map) {
+			this.map = map;
+		}
+
+		double asDouble(String key) {
+			return Double.parseDouble(map.get(key).toString());
+		}
+	}
 }
