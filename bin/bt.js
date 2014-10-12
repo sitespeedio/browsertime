2f50edef580f3e1b816d3b2cabe5dcbb162db920
soulgalore <peter@soulgalore.com>
2014-10-12 21:33:15 +0200

    fixing better input parameter names

--- bin/bt.js
+++ bin/bt.js
@@ -2,7 +2,14 @@
 
 var BT = require('../lib/bt'),
   bt = new BT(),
-  argv = require('minimist')(process.argv.slice(2));
+  argv = require('minimist')(process.argv.slice(2), opts = {
+    alias: {
+      'u': 'url',
+      'b': 'browser',
+      'n': 'runs',
+      's': 'size'
+    }
+  });
 
 bt.fetch(
   argv
