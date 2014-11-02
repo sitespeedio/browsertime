c35ce15a21da6829955cc72bfe57fddc3a5ac1f8
soulgalore <peter@soulgalore.com>
2014-11-02 22:05:42 +0100

    check that Java exists

--- bin/browsertime.js
+++ bin/browsertime.js
@@ -1,20 +1,28 @@
 #!/usr/bin/env node
 
-var Browsertime = require('../lib/browsertime'),
-cli = require('../lib/cli'),
-  bt = new Browsertime(),
-  argv = require('minimist')(process.argv.slice(2), {
-    alias: {
-      'u': 'url',
-      'b': 'browser',
-      'n': 'runs',
-      'w': 'size',
-      'f': 'filename'
-    }
-  });
+require('whereis')('java', function searched(err) {
+  // yep, we need Java for Selenium & the proxy
+  if (err) {
+    console.error(
+      'Could not find Java, make sure it is installed in your $PATH');
+  } else {
+    var Browsertime = require('../lib/browsertime'),
+      cli = require('../lib/cli'),
+      bt = new Browsertime(),
+      argv = require('minimist')(process.argv.slice(2), {
+        alias: {
+          'u': 'url',
+          'b': 'browser',
+          'n': 'runs',
+          'w': 'size',
+          'f': 'filename'
+        }
+      });
 
-cli.verifyInput(argv);
+    cli.verifyInput(argv);
 
-bt.fetch(
-  argv
-);
+    bt.fetch(
+      argv
+    );
+  }
+});
