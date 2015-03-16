0792abaa2886dde255c890651f5b9108eace9ab7
soulgalore <peter@soulgalore.com>
2015-03-16 14:14:59 +0100

    removing static stats, let them all be dynamic #81

--- scripts/static/documentHeight.js
+++
@@ -1,7 +0,0 @@
-function docHeight(doc) {
-	var body = doc.body, docelem = doc.documentElement;
-	var height = Math.max( body.scrollHeight, body.offsetHeight, docelem.clientHeight, docelem.scrollHeight, docelem.offsetHeight );
-	return height;
-}
-
-return docHeight(document);
