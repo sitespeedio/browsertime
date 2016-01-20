d4a73e4946c46d28729d5544b8101264b04a79da
Tobias Lidskog <tobias@lidskog.se>
2016-01-20 19:09:27 +0100

    Allow merging of har files.

--- test/har_builder_tests.js
+++ test/har_builder_tests.js
@@ -14,7 +14,13 @@ describe('har_builder', function() {
           'name': 'Chrome',
           'version': '42.0.2311.90',
           'comment': ''
-        }
+        },
+        'pages': [{
+          'id': 'page_0'
+        }],
+        'entries': [{
+          'pageref': 'page_0'
+        }]
       }
     };
   });
@@ -25,6 +31,62 @@ describe('har_builder', function() {
 
       expect(har).to.have.deep.property('log.creator.name', 'Browsertime');
       expect(har).to.have.deep.property('log.creator.comment', 'foo');
+    });
+
+    it('should not add comment to creator unless specified', function() {
+      builder.addCreator(har);
+
+      expect(har).to.have.deep.property('log.creator.name', 'Browsertime');
+      expect(har).to.not.have.deep.property('log.creator.comment');
+    });
+
+    it('should not add empty comment to creator', function() {
+      builder.addCreator(har, '');
+
+      expect(har).to.have.deep.property('log.creator.name', 'Browsertime');
+      expect(har).to.not.have.deep.property('log.creator.comment');
+    });
+  });
+
+  describe('#mergeHars', function() {
+    it('should merge two hars', function() {
+      builder.addCreator(har);
+
+      let har2 = {
+        'log': {
+          'version': 'kjsdhfksa',
+          'browser': {
+            'name': 'jksdhfjksd',
+            'version': 'jkdhfkjsdfdkjsf',
+            'comment': ''
+          },
+          'pages': [{
+            'id': 'page_0'
+          }],
+          'entries': [{
+            'pageref': 'page_0'
+          }]
+        }
+      };
+
+      let combinedHar = builder.mergeHars([har, har2]);
+
+      expect(combinedHar.log.pages).to.eql([
+        {
+          'id': 'page_0'
+        },
+        {
+          'id': 'page_0-1'
+        }]);
+      expect(combinedHar.log.entries).to.eql([
+        {
+          'pageref': 'page_0'
+        },
+        {
+          'pageref': 'page_0-1'
+        }]);
+      expect(combinedHar).to.have.deep.property('log.version', '1.2');
+      expect(combinedHar).to.have.deep.property('log.creator.name', 'Browsertime');
     });
   });
 });
