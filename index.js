54d37d671e4de292ec5a8d9cf09f8226db4b2221
Tobias Lidskog <tobias@lidskog.se>
2016-02-15 15:57:54 +0100

    Check for failed loads, and throw custom error.
    Note that both Chrome and Firefox doesn't fail if a non-existing url is loaded (e.g. http://kldsjflkajlkdfjalöksjfd.dfkljslkdl). This would lead to hard to debug errors.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use strict';
 
-let Engine = require('./lib/core/engine'),
+const Engine = require('./lib/core/engine'),
   SeleniumRunner = require('./lib/core/selenium_runner'),
+  errors = require('./lib/support/errors'),
   browserScripts = require('./lib/support/browser_script'),
   logging = require('./lib/support/logging');
 
 module.exports = {
-  Engine, SeleniumRunner, logging, browserScripts
+  Engine, SeleniumRunner, errors, logging, browserScripts
 };
