b08058dd0085f1b0c39f508f09f4259510d3bc75
soulgalore <peter@soulgalore.com>
2016-06-07 07:08:14 +0200

    latest rumSpeedIndex with fix for latest Chrome

--- browserscripts/timings/rumSpeedIndex.js
+++ browserscripts/timings/rumSpeedIndex.js
@@ -1,69 +1,71 @@
 (function() {
   /******************************************************************************
-   Copyright (c) 2014, Google Inc.
-   All rights reserved.
-   Redistribution and use in source and binary forms, with or without
-   modification, are permitted provided that the following conditions are met:
-   * Redistributions of source code must retain the above copyright notice,
-   this list of conditions and the following disclaimer.
-   * Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution.
-   * Neither the name of the <ORGANIZATION> nor the names of its contributors
-   may be used to endorse or promote products derived from this software
-   without specific prior written permission.
-   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-   FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-   DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-   SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-   CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-   OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-   ******************************************************************************/
+  Copyright (c) 2014, Google Inc.
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+      * Redistributions of source code must retain the above copyright notice,
+        this list of conditions and the following disclaimer.
+      * Redistributions in binary form must reproduce the above copyright notice,
+        this list of conditions and the following disclaimer in the documentation
+        and/or other materials provided with the distribution.
+      * Neither the name of the <ORGANIZATION> nor the names of its contributors
+      may be used to endorse or promote products derived from this software
+      without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  ******************************************************************************/
 
   /******************************************************************************
-   *******************************************************************************
-   Calculates the Speed Index for a page by:
-   - Collecting a list of visible rectangles for elements that loaded
-   external resources (images, background images, fonts)
-   - Gets the time when the external resource for those elements loaded
-   through Resource Timing
-   - Calculates the likely time that the background painted
-   - Runs the various paint rectangles through the SpeedIndex calculation:
-   https://sites.google.com/a/webpagetest.org/docs/using-webpagetest/metrics/speed-index
-   TODO:
-   - Improve the start render estimate
-   - Handle overlapping rects (though maybe counting the area as multiple paints
-   will work out well)
-   - Detect elements with Custom fonts and the time that the respective font
-   loaded
-   - Better error handling for browsers that don't support resource timing
-   *******************************************************************************
-   ******************************************************************************/
+  *******************************************************************************
+    Calculates the Speed Index for a page by:
+    - Collecting a list of visible rectangles for elements that loaded
+      external resources (images, background images, fonts)
+    - Gets the time when the external resource for those elements loaded
+      through Resource Timing
+    - Calculates the likely time that the background painted
+    - Runs the various paint rectangles through the SpeedIndex calculation:
+      https://sites.google.com/a/webpagetest.org/docs/using-webpagetest/metrics/speed-index
+
+    TODO:
+    - Improve the start render estimate
+    - Handle overlapping rects (though maybe counting the area as multiple paints
+      will work out well)
+    - Detect elements with Custom fonts and the time that the respective font
+      loaded
+    - Better error handling for browsers that don't support resource timing
+  *******************************************************************************
+  ******************************************************************************/
 
   var RUMSpeedIndex = function(win) {
     win = win || window;
     var doc = win.document;
 
     /****************************************************************************
-     Support Routines
-     ****************************************************************************/
+      Support Routines
+    ****************************************************************************/
     // Get the rect for the visible portion of the provided DOM element
     var GetElementViewportRect = function(el) {
       var intersect = false;
       if (el.getBoundingClientRect) {
         var elRect = el.getBoundingClientRect();
-        intersect = {
-          'top': Math.max(elRect.top, 0),
-          'left': Math.max(elRect.left, 0),
-          'bottom': Math.min(elRect.bottom, (win.innerHeight || doc.documentElement.clientHeight)),
-          'right': Math.min(elRect.right, (win.innerWidth || doc.documentElement.clientWidth))
-        };
+        intersect = {'top': Math.max(elRect.top, 0),
+                         'left': Math.max(elRect.left, 0),
+                         'bottom': Math.min(elRect.bottom, (win.innerHeight || doc.documentElement.clientHeight)),
+                         'right': Math.min(elRect.right, (win.innerWidth || doc.documentElement.clientWidth))};
         if (intersect.bottom <= intersect.top ||
-          intersect.right <= intersect.left) {
+            intersect.right <= intersect.left) {
           intersect = false;
         } else {
           intersect.area = (intersect.bottom - intersect.top) * (intersect.right - intersect.left);
@@ -77,11 +79,9 @@
       if (url) {
         var rect = GetElementViewportRect(el);
         if (rect) {
-          rects.push({
-            'url': url,
-            'area': rect.area,
-            'rect': rect
-          });
+          rects.push({'url': url,
+                       'area': rect.area,
+                       'rect': rect});
         }
       }
     };
@@ -90,7 +90,7 @@
     var GetRects = function() {
       // Walk all of the elements in the DOM (try to only do this once)
       var elements = doc.getElementsByTagName('*');
-      var re = /url\((http.*)\)/ig;
+      var re = /url\(.*(http.*)\)/ig;
       for (var i = 0; i < elements.length; i++) {
         var el = elements[i];
         var style = win.getComputedStyle(el);
@@ -104,7 +104,7 @@
           re.lastIndex = 0;
           var matches = re.exec(style['background-image']);
           if (matches && matches.length > 1)
-            CheckElement(el, matches[1]);
+            CheckElement(el, matches[1].replace('"', ''));
         }
         // recursively walk any iFrames
         if (el.tagName == 'IFRAME') {
@@ -113,14 +113,12 @@
             if (rect) {
               var tm = RUMSpeedIndex(el.contentWindow);
               if (tm) {
-                rects.push({
-                  'tm': tm,
-                  'area': rect.area,
-                  'rect': rect
-                });
+                rects.push({'tm': tm,
+                            'area': rect.area,
+                            'rect': rect});
               }
-            }
-          } catch (e) { // hepp
+          }
+          } catch(e) {
           }
         }
       }
@@ -170,8 +168,8 @@
         var doneCritical = false;
         for (var j = 0; j < requests.length; j++) {
           if (!doneCritical &&
-            headURLs[requests[j].name] &&
-            (requests[j].initiatorType == 'script' || requests[j].initiatorType == 'link')) {
+              headURLs[requests[j].name] &&
+             (requests[j].initiatorType == 'script' || requests[j].initiatorType == 'link')) {
             var requestEnd = requests[j].responseEnd;
             if (firstPaint === undefined || requestEnd > firstPaint)
               firstPaint = requestEnd;
@@ -186,7 +184,7 @@
     // Sort and group all of the paint rects by time and use them to
     // calculate the visual progress
     var CalculateVisualProgress = function() {
-      var paints = {'0': 0};
+      var paints = {'0':0};
       var total = 0;
       for (var i = 0; i < rects.length; i++) {
         var tm = firstPaint;
@@ -200,8 +198,8 @@
       // Add a paint area for the page background (count 10% of the pixels not
       // covered by existing paint rects.
       var pixels = Math.max(doc.documentElement.clientWidth, win.innerWidth || 0) *
-        Math.max(doc.documentElement.clientHeight, win.innerHeight || 0);
-      if (pixels > 0) {
+                   Math.max(doc.documentElement.clientHeight, win.innerHeight || 0);
+      if (pixels > 0 ) {
         pixels = Math.max(pixels - total, 0) * pageBackgroundWeight;
         if (paints[firstPaint] === undefined)
           paints[firstPaint] = 0;
@@ -215,9 +213,7 @@
             progress.push({'tm': time, 'area': paints[time]});
           }
         }
-        progress.sort(function(a, b) {
-          return a.tm - b.tm;
-        });
+        progress.sort(function(a,b){return a.tm - b.tm;});
         var accumulated = 0;
         for (var j = 0; j < progress.length; j++) {
           accumulated += progress[j].area;
@@ -245,8 +241,8 @@
     };
 
     /****************************************************************************
-     Main flow
-     ****************************************************************************/
+      Main flow
+    ****************************************************************************/
     var rects = [];
     var progress = [];
     var firstPaint;
@@ -259,22 +255,21 @@
       GetFirstPaint();
       CalculateVisualProgress();
       CalculateSpeedIndex();
-    } catch (e) { // hepp
+    } catch(e) {
     }
     /* Debug output for testing
-     var dbg = '';
-     dbg += "Paint Rects\n";
-     for (var i = 0; i < rects.length; i++)
-     dbg += '(' + rects[i].area + ') ' + rects[i].tm + ' - ' + rects[i].url + "\n";
-     dbg += "Visual Progress\n";
-     for (var i = 0; i < progress.length; i++)
-     dbg += '(' + progress[i].area + ') ' + progress[i].tm + ' - ' + progress[i].progress + "\n";
-     dbg += 'First Paint: ' + firstPaint + "\n";
-     dbg += 'Speed Index: ' + SpeedIndex + "\n";
-     console.log(dbg);
-     */
+    var dbg = '';
+    dbg += "Paint Rects\n";
+    for (var i = 0; i < rects.length; i++)
+      dbg += '(' + rects[i].area + ') ' + rects[i].tm + ' - ' + rects[i].url + "\n";
+    dbg += "Visual Progress\n";
+    for (var i = 0; i < progress.length; i++)
+      dbg += '(' + progress[i].area + ') ' + progress[i].tm + ' - ' + progress[i].progress + "\n";
+    dbg += 'First Paint: ' + firstPaint + "\n";
+    dbg += 'Speed Index: ' + SpeedIndex + "\n";
+    console.log(dbg);
+    */
     return SpeedIndex;
   };
-
   return RUMSpeedIndex()|| -1;
 })();
