29c181cfa56c57f8fa3490fcdf77d5dcd540c8ea
soulgalore <peter@soulgalore.com>
2016-08-09 21:10:16 +0200

    remove test case

--- test/tsproxytests.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-bin/browsertime.js https://www.sitespeed.io --connectivity.profile cable -n 1
-bin/browsertime.js http://www.aftonbladet.se --connectivity.profile cable -n 1
-bin/browsertime.js https://www.sitespeed.io --connectivity.profile 3g -n 1
