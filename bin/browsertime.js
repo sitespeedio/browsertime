b8a1fa12d835f46bd9efd1e0260c611473cb9d55
moos <mooster@42at.com>
2016-09-25 00:04:02 -0700

    Add chrome-esque summary line.  Disable with double -q.

--- bin/browsertime.js
+++ bin/browsertime.js
@@ -27,6 +27,11 @@ function parseUserScripts(scripts) {
     {});
 }
 
+// 1200 -> 1.2
+function fmt(msec, d=2) {
+  return (msec/1000).toFixed(d).replace(/.0+$/,'');
+}
+
 function run(url, options) {
   let dir = 'browsertime-results';
   if (!fs.existsSync(dir)) {
@@ -72,8 +77,32 @@ function run(url, options) {
         saveOperations.push(storageManager.writeData(`screenshot-${index}.png`, value)));
 
       return Promise.all(saveOperations)
-        .then(() => log.info('Wrote data to %s',
-          path.relative(process.cwd(), storageManager.directory)));
+        .then(() => {
+          log.info('Wrote data to %s', path.relative(process.cwd(), storageManager.directory));
+          return result;
+        });
+    })
+    .tap((result) => {
+      // don't bother if no statistics or silent x2
+      if (!result.statistics || options.silent > 1) return result;
+
+      var run = result.browserScripts[0].timings,
+        nRuns = result.browserScripts.length,
+        st = result.statistics.timings,
+        nav = st.navigationTiming,
+        domContentLoadedTime = nav.domContentLoadedEventStart.mean - nav.navigationStart.mean,
+        m = nRuns > 1,
+        lines = [
+          `${run.resourceTimings.length} requests`,
+          `DOMContentLoaded: ${fmt(domContentLoadedTime)}s` + (m ? ` (±${fmt(nav.domContentLoadedEventStart.mdev)})` : ''),
+          `Load: ${fmt(st.fullyLoaded.mean)}s` + (m ? ` (±${fmt(st.fullyLoaded.mdev)})` : ''),
+          `rumSpeedIndex: ${st.rumSpeedIndex.mean}` + (m ? ` (±${st.rumSpeedIndex.mdev})` : '')
+        ],
+        note = m ? ` (${nRuns} runs)` : '';
+
+      lines = lines.join(', ');
+      console.log(`${lines}${note}`);  // eslint-disable-line no-console
+      return result;
     })
     .catch(function(e) {
       log.error('Error running browsertime', e);
