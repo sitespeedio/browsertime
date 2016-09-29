a850890b44ae8c6deef359cf5bff519f829f34f4
soulgalore <peter@soulgalore.com>
2016-09-29 14:17:24 +0200

    goodbye raw config

--- test/trafficShapeParserTests.js
+++ test/trafficShapeParserTests.js
@@ -6,20 +6,6 @@ let parser = require('../lib/support/trafficShapeParser'),
 describe('traffic_shape_parser', function() {
   describe('#parseTrafficShapeConfig', function() {
 
-
-    it('should convert the raw config', function() {
-      let rawConfig = "{\"downstreamKbps\": 42, \"upstreamKbps\": 18, \"latency\": 37}";
-
-      let shapeConfig = parser.parseTrafficShapeConfig({
-        connectivity: {
-          config: rawConfig
-        }
-      });
-      shapeConfig.downstreamKbps.should.equal(42);
-      shapeConfig.upstreamKbps.should.equal(18);
-      shapeConfig.latency.should.equal(37);
-    });
-
     let profiles = parser.getProfiles();
 
     Object.keys(profiles).forEach(function(name) {
