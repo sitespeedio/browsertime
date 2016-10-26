c7d8660013b8c83c57e727ff1f5c470ac8826635
soulgalore <peter@soulgalore.com>
2016-10-26 08:06:29 +0200

    release me

---
+++ release.sh
@@ -0,0 +1,15 @@
+ #!/bin/bash
+set -e
+# Super simple release script for browsertime
+# Lets use it it for now and make it better over time :)
+# You need np for this to work
+# npm install --global np
+np $1
+
+PACKAGE_VERSION=$(grep -m1 version package.json | awk -F: '{ print $2 }' | sed 's/[", ]//g')
+
+docker build --no-cache -t sitespeedio/browsertime:$PACKAGE_VERSION .
+
+docker login
+
+docker push sitespeedio/browsertime:$PACKAGE_VERSION
