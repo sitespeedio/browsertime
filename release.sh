aac0a53afb62b07efb628a1c02308d2ea5435cde
soulgalore <peter@soulgalore.com>
2016-10-28 20:55:40 +0200

    Revert "new release script lets use it when we release 1.0"
    This reverts commit ece71b05a6ca58d449d6c48e7e8ff882deb26422.

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
  #!/bin/bash
 set -e
-# Super simple release script for Browsertime
+# Super simple release script for browsertime
 # Lets use it it for now and make it better over time :)
 # You need np for this to work
 # npm install --global np
@@ -8,9 +8,8 @@ np $1
 
 PACKAGE_VERSION=$(grep -m1 version package.json | awk -F: '{ print $2 }' | sed 's/[", ]//g')
 
-docker build --no-cache -t sitespeedio/sitespeed.io:$PACKAGE_VERSION -t sitespeedio/sitespeed.io:latest .
+docker build --no-cache -t sitespeedio/browsertime:$PACKAGE_VERSION .
 
 docker login
 
-docker push sitespeedio/sitespeed.io:$PACKAGE_VERSION
-docker push sitespeedio/sitespeed.io:latest
+docker push sitespeedio/browsertime:$PACKAGE_VERSION
