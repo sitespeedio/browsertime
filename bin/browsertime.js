08c6168bc3d168130b785723c026023a6b491fd1
Leray Julien <leray.jul@gmail.com>
2017-03-06 15:08:25 +0100

    Har file to Mongo

--- bin/browsertime.js
+++ bin/browsertime.js
@@ -6,6 +6,7 @@ let Engine = require('../').Engine,
   logging = require('../').logging,
   cli = require('../lib/support/cli'),
   StorageManager = require('../lib/support/storageManager'),
+  DatabaseManager = require('../lib/support/databaseManager'),
   Promise = require('bluebird'),
   merge = require('lodash.merge'),
   isEmpty = require('lodash.isempty'),
@@ -56,26 +57,34 @@ engine.start()
     .then(function(result) {
       let saveOperations = [];
 
-      const storageManager = new StorageManager(url, options);
-      const harName = (options.har) ? (options.har) : 'browsertime';
-      const jsonName = (options.output) ? (options.output) : 'browsertime';
-      const btData = pick(result, ['info', 'browserScripts', 'statistics', 'visualMetrics']);
-      if (!isEmpty(btData)) {
-        saveOperations.push(storageManager.writeJson(jsonName + '.json', btData));
-      }
-      if (result.har) {
-        saveOperations.push(storageManager.writeJson(harName + '.har', result.har));
-      }
-      forEach(result.extraJson, (value, key) =>
-        saveOperations.push(storageManager.writeJson(key, value)));
-      forEach(result.screenshots, (value, index) =>
-        saveOperations.push(storageManager.writeData(`screenshot-${index}.png`, value)));
-
-      return Promise.all(saveOperations)
-        .then(() => {
-          log.info('Wrote data to %s', path.relative(process.cwd(), storageManager.directory));
+      if(options.mongo.URI){
+        const databaseManager = new DatabaseManager(options);
+        return databaseManager.saveToMongo(result).then(() => {
+          log.info('Wrote data to Mongo');
           return result;
         });
+      } else {
+        const storageManager = new StorageManager(url, options);
+        const harName = (options.har) ? (options.har) : 'browsertime';
+        const jsonName = (options.output) ? (options.output) : 'browsertime';
+        const btData = pick(result, ['info', 'browserScripts', 'statistics', 'visualMetrics']);
+        if (!isEmpty(btData)) {
+          saveOperations.push(storageManager.writeJson(jsonName + '.json', btData));
+        }
+        if (result.har) {
+          saveOperations.push(storageManager.writeJson(harName + '.har', result.har));
+        }
+        forEach(result.extraJson, (value, key) =>
+          saveOperations.push(storageManager.writeJson(key, value)));
+        forEach(result.screenshots, (value, index) =>
+          saveOperations.push(storageManager.writeData(`screenshot-${index}.png`, value)));
+
+        return Promise.all(saveOperations)
+          .then(() => {
+            log.info('Wrote data to %s', path.relative(process.cwd(), storageManager.directory));
+            return result;
+          });
+      }
     })
     .catch(function(e) {
       log.error('Error running browsertime', e);
