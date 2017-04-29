71d56f9a752df26f88ad70653d74dfad76a57fa6
soulgalore <peter@soulgalore.com>
2017-04-29 15:17:11 +0200

    first steps

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
-FROM sitespeedio/webbrowsers:firefox-53.0-chrome-58.0
- 
+FROM sitespeedio/webbrowsers:firefox-53.0-chrome-58.0-2
+
 ENV BROWSERTIME_XVFB true
 ENV BROWSERTIME_CONNECTIVITY__ENGINE tc
 ENV BROWSERTIME_CHROME__ARGS no-sandbox
--- docker/scripts/start.sh
+++ docker/scripts/start.sh
@@ -14,6 +14,13 @@ service dbus status > /dev/null
 export $(dbus-launch)
 export NSS_USE_SHARED_DB=ENABLED
 
+# Start adb server and list connected devices
+#START_ADB_SERVER="${START_ADB_SERVER:-false}"
+if [ -n "$START_ADB_SERVER" ] ; then
+  sudo adb start-server
+  sudo adb devices
+fi
+
 # Inspired by docker-selenium way of shutting down
 function shutdown {
   kill -s SIGTERM ${PID}
