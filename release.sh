c630b35c24ae756f264f8581211af10c540bed54
Peter Hedenskog <phedenskog@wikimedia.org>
2017-12-05 13:43:24 +0100

    fixed line break

--- release.sh
+++ release.sh
@@ -23,7 +23,6 @@ docker push sitespeedio/browsertime:latest
 
 bin/browsertime.js --version > ../sitespeed.io/docs/version/browsertime.txt
 
-docker build --no-cache -t sitespeedio/browsertime-wpr:${PACKAGE_VERSION} -t sitesp\
-eedio/browsertime-wpr:latest
+docker build --no-cache -t sitespeedio/browsertime-wpr:${PACKAGE_VERSION} -t sitespeedio/browsertime-wpr:latest
 docker push sitespeedio/browsertime-wpr:${PACKAGE_VERSION}
 docker push sitespeedio/browsertime-wpr:latest 
