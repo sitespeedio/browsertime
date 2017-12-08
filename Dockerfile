8521f028a6d2d7c928ab24cc2794c772c2123017
Peter Hedenskog <phedenskog@wikimedia.org>
2017-12-08 06:58:04 +0100

    include WPR in the default Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM sitespeedio/webbrowsers:firefox-54.0-chrome-62.0-2
+FROM sitespeedio/webbrowsers:firefox-57.0-chrome-63.0
 
 ENV BROWSERTIME_XVFB true
 ENV BROWSERTIME_CONNECTIVITY__ENGINE external
@@ -19,6 +19,29 @@ COPY . /usr/src/app
 RUN mkdir -m 0750 /root/.android
 ADD docker/adb/insecure_shared_adbkey /root/.android/adbkey
 ADD docker/adb/insecure_shared_adbkey.pub /root/.android/adbkey.pub
+
+# Install Go, WebPageReplay and the webpagereplay wrapper
+
+WORKDIR /work
+RUN sudo apt-get update && sudo apt-get install libnss3-tools \
+  curl \
+  git \
+  iproute2 -y && \
+  mkdir -p $HOME/.pki/nssdb && \
+  certutil -d $HOME/.pki/nssdb -N && \
+  curl -O https://storage.googleapis.com/golang/go1.9.linux-amd64.tar.gz && \
+  tar -xvf go1.9.linux-amd64.tar.gz && \
+  sudo mv go /usr/local
+
+ENV PATH="/usr/local/go/bin:${PATH}"
+
+RUN go get github.com/urfave/cli && \
+  go get golang.org/x/net/http2 && \
+  go get github.com/catapult-project/catapult/web_page_replay_go/src/webpagereplay && \
+  npm install webpagereplaywrapper -g
+
+WORKDIR /root/go/src/github.com/catapult-project/catapult/web_page_replay_go
+RUN go run src/wpr.go installroot
 
 WORKDIR /browsertime
 
--- docker/scripts/start.sh
+++ docker/scripts/start.sh
@@ -4,31 +4,77 @@ set -e
 google-chrome --version
 firefox --version
 
-# Here's a hack for fixing the problem with Chrome not starting in time
-# See https://github.com/SeleniumHQ/docker-selenium/issues/87#issuecomment-250475864
+function runWebPageReplay(){
+  RUNS="${RUNS:-5}"
+  LATENCY=${LATENCY:-100}
+  BROWSER=${BROWSER:-'chrome'}
+  HTTP_PORT=80
+  HTTPS_PORT=443
 
-sudo rm -f /var/lib/dbus/machine-id
-sudo mkdir -p /var/run/dbus
-sudo service dbus restart > /dev/null
-service dbus status > /dev/null
-export $(dbus-launch)
-export NSS_USE_SHARED_DB=ENABLED
+  webpagereplaywrapper record --start --path /root/go/src/github.com/catapult-project/catapult/web_page_replay_go --http $HTTP_PORT --https $HTTPS_PORT
 
-# Start adb server and list connected devices
-if [ -n "$START_ADB_SERVER" ] ; then
-  sudo adb start-server
-  sudo adb devices
-fi
+  if [ $BROWSER = 'chrome' ]
+  then
+      /usr/src/app/bin/browsertime.js -b $BROWSER -n 1 --chrome.args host-resolver-rules="MAP *:$HTTP_PORT 127.0.0.1:$HTTP_PORT,MAP *:$HTTPS_PORT 127.0.0.1:$HTTPS_PORT,EXCLUDE localhost" --pageCompleteCheck "return true;" "$@"
+  else
+      /usr/src/app/bin/browsertime.js -b $BROWSER -n 1 --firefox.preference network.dns.forceResolve:127.0.0.1 --firefox.acceptInsecureCerts --skipHar --pageCompleteCheck "return true;" "$@"
+  fi
 
-# Inspired by docker-selenium way of shutting down
-function shutdown {
-  kill -s SIGTERM ${PID}
+  webpagereplaywrapper record --stop --path /root/go/src/github.com/catapult-project/catapult/web_page_replay_go
+
+  webpagereplaywrapper replay --start --path /root/go/src/github.com/catapult-project/catapult/web_page_replay_go --http $HTTP_PORT--https $HTTPS_PORT
+
+  if [ $BROWSER = 'chrome' ]
+  then
+      /usr/src/app/bin/browsertime.js -b $BROWSER -n $RUNS --chrome.args host-resolver-rules="MAP *:$HTTP_PORT 127.0.0.1:$HTTP_PORT,MAP *:$HTTPS_PORT 127.0.0.1:$HTTPS_PORT,EXCLUDE localhost" --video --speedIndex --pageCompleteCheck "return true;" --connectivity.engine throttle --connectivity.throttle.localhost --connectivity.profile custom --connectivity.latency $LATENCY "$@"
+  else
+      /usr/src/app/bin/browsertime.js -b $BROWSER -n $RUNS --firefox.preference network.dns.forceResolve:127.0.0.1 --video --speedIndex --pageCompleteCheck "return true;" --connectivity.engine throttle --connectivity.throttle.localhost --connectivity.profile custom --connectivity.latency $LATENCY --skipHar --firefox.acceptInsecureCerts "$@"
+  fi
+
+  webpagereplaywrapper replay --stop --path /root/go/src/github.com/catapult-project/catapult/web_page_replay_go
+}
+
+function chromeSetup() {
+  # Here's a hack for fixing the problem with Chrome not starting in time
+  # See https://github.com/SeleniumHQ/docker-selenium/issues/87#issuecomment-250475864
+
+  sudo rm -f /var/lib/dbus/machine-id
+  sudo mkdir -p /var/run/dbus
+  sudo service dbus restart > /dev/null
+  service dbus status > /dev/null
+  export $(dbus-launch)
+  export NSS_USE_SHARED_DB=ENABLED
+}
+
+function setupADB(){
+  # Start adb server and list connected devices
+  if [ -n "$START_ADB_SERVER" ] ; then
+    sudo adb start-server
+    sudo adb devices
+  fi
+}
+
+function runBrowsertime(){
+
+  # Inspired by docker-selenium way of shutting down
+  function shutdown {
+    kill -s SIGTERM ${PID}
+    wait $PID
+  }
+
+  exec /usr/src/app/bin/browsertime.js "$@" &
+
+  PID=$!
+
+  trap shutdown SIGTERM SIGINT
   wait $PID
 }
 
-exec /usr/src/app/bin/browsertime.js "$@" &
-
-PID=$!
-
-trap shutdown SIGTERM SIGINT
-wait $PID
+chromeSetup
+setupADB
+if [ $REPLAY ]
+then
+  runWebPageReplay $@
+else
+  runBrowsertime $@
+fi
