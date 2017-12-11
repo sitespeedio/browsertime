1d56a88029ee46fa0bd306ac6807326699ae665e
Peter Hedenskog <peter@soulgalore.com>
2017-12-11 21:29:09 +0100

    Docker file with WebPageReplay (#410)

--- docker/scripts/start.sh
+++ docker/scripts/start.sh
@@ -4,31 +4,84 @@ set -e
 google-chrome --version
 firefox --version
 
+BROWSERTIME=/usr/src/app/bin/browsertime.js
+
 # Here's a hack for fixing the problem with Chrome not starting in time
 # See https://github.com/SeleniumHQ/docker-selenium/issues/87#issuecomment-250475864
+function chromeSetup() {
+  sudo rm -f /var/lib/dbus/machine-id
+  sudo mkdir -p /var/run/dbus
+  sudo service dbus restart > /dev/null
+  service dbus status > /dev/null
+  export $(dbus-launch)
+  export NSS_USE_SHARED_DB=ENABLED
+}
 
-sudo rm -f /var/lib/dbus/machine-id
-sudo mkdir -p /var/run/dbus
-sudo service dbus restart > /dev/null
-service dbus status > /dev/null
-export $(dbus-launch)
-export NSS_USE_SHARED_DB=ENABLED
+# If we run Chrome on Android, we need to start the ADB server
+function setupADB(){
+  # Start adb server and list connected devices
+  if [ -n "$START_ADB_SERVER" ] ; then
+    sudo adb start-server
+    sudo adb devices
+  fi
+}
 
-# Start adb server and list connected devices
-if [ -n "$START_ADB_SERVER" ] ; then
-  sudo adb start-server
-  sudo adb devices
-fi
+function runWebPageReplay() {
 
-# Inspired by docker-selenium way of shutting down
-function shutdown {
-  kill -s SIGTERM ${PID}
+  RUNS="${RUNS:-5}"
+  LATENCY=${LATENCY:-100}
+  BROWSER=${BROWSER:-'chrome'}
+  HTTP_PORT=80
+  HTTPS_PORT=443
+  WPR_PATH=/root/go/src/github.com/catapult-project/catapult/web_page_replay_go
+  WPR_PARAMS="--path $WPR_PATH --http $HTTP_PORT --https $HTTPS_PORT"
+
+  webpagereplaywrapper record --start $WPR_PARAMS
+
+  if [ $BROWSER = 'chrome' ]
+  then
+    $BROWSERTIME -b $BROWSER -n 1 --chrome.args host-resolver-rules="MAP *:$HTTP_PORT 127.0.0.1:$HTTP_PORT,MAP *:$HTTPS_PORT 127.0.0.1:$HTTPS_PORT,EXCLUDE localhost" --pageCompleteCheck "return true;" "$@"
+  else
+    $BROWSERTIME -b $BROWSER -n 1 --firefox.preference network.dns.forceResolve:127.0.0.1 --firefox.acceptInsecureCerts --skipHar --pageCompleteCheck "return true;" "$@"
+  fi
+
+  webpagereplaywrapper record --stop $WPR_PARAMS
+
+  webpagereplaywrapper replay --start $WPR_PARAMS
+
+  if [ $BROWSER = 'chrome' ]
+  then
+    $BROWSERTIME -b $BROWSER -n $RUNS --chrome.args host-resolver-rules="MAP *:$HTTP_PORT 127.0.0.1:$HTTP_PORT,MAP *:$HTTPS_PORT 127.0.0.1:$HTTPS_PORT,EXCLUDE localhost" --video --speedIndex --pageCompleteCheck "return true;" --connectivity.engine throttle --connectivity.throttle.localhost --connectivity.profile custom --connectivity.latency $LATENCY "$@"
+  else
+    $BROWSERTIME -b $BROWSER -n $RUNS --firefox.preference network.dns.forceResolve:127.0.0.1 --video --speedIndex --pageCompleteCheck "return true;" --connectivity.engine throttle --connectivity.throttle.localhost --connectivity.profile custom --connectivity.latency $LATENCY --skipHar --firefox.acceptInsecureCerts "$@"
+  fi
+
+  webpagereplaywrapper replay --stop $WPR_PARAMS
+}
+
+
+function runBrowsertime(){
+
+  # Inspired by docker-selenium way of shutting down
+  function shutdown {
+    kill -s SIGTERM ${PID}
+    wait $PID
+  }
+
+  exec $BROWSERTIME "$@" &
+
+  PID=$!
+
+  trap shutdown SIGTERM SIGINT
   wait $PID
 }
 
-exec /usr/src/app/bin/browsertime.js "$@" &
+chromeSetup
+setupADB
 
-PID=$!
-
-trap shutdown SIGTERM SIGINT
-wait $PID
+if [ $REPLAY ]
+then
+  runWebPageReplay $@
+else
+  runBrowsertime $@
+fi
--- release.sh
+++ release.sh
@@ -22,3 +22,6 @@ docker push sitespeedio/browsertime:${PACKAGE_VERSION}
 docker push sitespeedio/browsertime:latest
 
 bin/browsertime.js --version > ../sitespeed.io/docs/version/browsertime.txt
+
+docker build -f Dockerfile.wpr --no-cache -t sitespeedio/browsertime:${PACKAGE_VERSION}-wpr-alpha .
+docker push sitespeedio/browsertime:${PACKAGE_VERSION}-wpr-alpha
