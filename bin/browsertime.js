ffb73ecb8d2da75c5c7771b4e6709466aa3c3501
Tobias Lidskog <tobias@lidskog.se>
2018-03-21 18:40:48 +0100

    Replace Bluebird usages with promisify.
    Slowly migrate from Bluebird promises to native javascript/node constructs.

--- bin/browsertime.js
+++ bin/browsertime.js
@@ -14,8 +14,6 @@ const fs = require('fs');
 const path = require('path');
 const log = require('intel');
 
-Promise.promisifyAll(fs);
-
 function parseUserScripts(scripts) {
   if (!Array.isArray(scripts)) scripts = [scripts];
 
