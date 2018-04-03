9dc1a081e77c68d41888a4a8932c36984ebdd5dc
soulgalore <peter@soulgalore.com>
2018-04-03 10:04:30 +0200

    do not push latest to docker until 3.0.0

--- release.sh
+++ release.sh
@@ -19,7 +19,7 @@ PACKAGE_VERSION=$(node -e 'console.log(require("./package").version)')
 docker build --no-cache -t sitespeedio/browsertime:${PACKAGE_VERSION} -t sitespeedio/browsertime:latest .
 
 docker push sitespeedio/browsertime:${PACKAGE_VERSION}
-docker push sitespeedio/browsertime:latest
+#docker push sitespeedio/browsertime:latest
 
 # bin/browsertime.js --version | tr -d '\n' > ../sitespeed.io/docs/_includes/version/browsertime.txt
 
