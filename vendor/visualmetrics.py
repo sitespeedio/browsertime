93e6da81ba66c34152bc0595c0d437bb035a4392
Peter Hedenskog <phedenskog@wikimedia.org>
2018-04-10 12:20:18 +0200

    use same bottom margin all over the place

--- vendor/visualmetrics.py
+++ vendor/visualmetrics.py
@@ -596,11 +596,11 @@ def eliminate_duplicate_frames(directory):
             # Figure out the region of the image that we care about
             top = 6
             right_margin = 6
-            bottom_margin = 6
+            bottom_margin = 25
             if height > 400 or width > 400:
                 top = int(math.ceil(float(height) * 0.03))
                 right_margin = int(math.ceil(float(width) * 0.03))
-                bottom_margin = int(math.ceil(float(width) * 0.03))
+                bottom_margin = int(math.ceil(float(width) * 0.04))
             height = max(height - top - bottom_margin, 1)
             left = 0
             width = max(width - right_margin, 1)
