3e4a111ef1ee91100244131480270e47364900bc
soulgalore <peter@soulgalore.com>
2019-01-07 14:03:49 +0100

    Helper objects to make it easier to navigate: measure click wait text
    and to get hold of selenium.*

--- test/navigationscript/measure.js
+++ test/navigationscript/measure.js
@@ -1,4 +1,4 @@
 module.exports = async function(context) {
   context.log.info('Running script navigation');
-  return context.h.measure('https://www.sitespeed.io/');
+  return context.measure.startAndNavigate('https://www.sitespeed.io/');
 };
--- test/navigationscript/navigateAndStartInTwoSteps.js
+++ test/navigationscript/navigateAndStartInTwoSteps.js
@@ -1,16 +1,17 @@
 module.exports = async function(context) {
-  await context.h.navigate('https://www.sitespeed.io');
+  await context.navigate('https://www.sitespeed.io');
   // we fetch the selenium webdriver from context
-  const webdriver = context.webdriver;
-  const driver = context.driver;
+  const webdriver = context.selenium.webdriver;
+  const driver = context.selenium.driver;
   // and get hold of some goodies we want to use
   const until = webdriver.until;
   const By = webdriver.By;
-  // before you start, make your username and password
-  await context.h.startMeasure('https://www.sitespeed.io');
+
   const docLink = driver.findElement(By.linkText('Documentation'));
+  // before you start, make your username and password
+  await context.measure.start('https://www.sitespeed.io');
   // Before we click on the link, start the measurement
   docLink.click();
   await driver.wait(until.elementLocated(By.linkText('Chrome-HAR')), 6000);
-  return context.h.collect();
+  return context.measure.stop();
 };
