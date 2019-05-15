c8a2ccd65b1fa0e27d44a2d1ed0457194ebd513d
soulgalore <peter@soulgalore.com>
2019-05-15 20:12:38 +0200

    Make it possible to define the length of a long task

--- browserscripts/pageinfo/longTask.js
+++ browserscripts/pageinfo/longTask.js
@@ -1,23 +1,24 @@
-(function() {
+(function(minLength) {
   if (window.__bt_longtask) {
     const cleaned = [];
     for (let event of window.__bt_longtask.e) {
-      const e = {};
-      e.duration = event.duration;
-      e.name = event.name;
-      e.startTime = event.startTime;
-      e.attribution = [];
-      for (let at of event.attribution) {
-        const a = {};
-        a.containerId = at.containerId;
-        a.containerName = at.containerName;
-        a.containerSrc = at.containerSrc;
-        a.containerType = at.containerType;
-        e.attribution.push(a);
+      if (event.duration >= minLength) {
+        const e = {};
+        e.duration = event.duration;
+        e.name = event.name;
+        e.startTime = event.startTime;
+        e.attribution = [];
+        for (let at of event.attribution) {
+          const a = {};
+          a.containerId = at.containerId;
+          a.containerName = at.containerName;
+          a.containerSrc = at.containerSrc;
+          a.containerType = at.containerType;
+          e.attribution.push(a);
+        }
+        cleaned.push(e);
       }
-      cleaned.push(e);
     }
-
     return cleaned;
   }
-})();
+})(arguments[arguments.length - 1]);
