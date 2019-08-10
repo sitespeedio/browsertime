715068b59396e9f5a1d85ab3feff75d64282ce58
soulgalore <peter@soulgalore.com>
2019-08-10 22:34:38 +0200

    copy the metrics

--- browserscripts/timings/largestContentfulPaint.js
+++ browserscripts/timings/largestContentfulPaint.js
@@ -3,6 +3,15 @@
   observer.observe({ type: 'largest-contentful-paint', buffered: true });
   const entries = observer.takeRecords();
   if (entries.length > 0) {
-    return entries[entries.length - 1];
+    const largestEntry = entries[entries.length - 1];
+    return {
+      duration: largestEntry.duration,
+      id: largestEntry.id,
+      url: largestEntry.url,
+      loadTime: largestEntry.loadTime.toFixed(0),
+      renderTime: largestEntry.renderTime.toFixed(0),
+      size: largestEntry.size,
+      startTime: largestEntry.startTime.toFixed(0)
+    };
   } else return;
 })();
