992e167bc64ecc9cbc717704f605aca6466e1602
Peter Hedenskog <peter@soulgalore.com>
2019-10-30 21:56:47 +0100

    Be safe when using document title #979 (#980)

--- browserscripts/pageinfo/documentTitle.js
+++ browserscripts/pageinfo/documentTitle.js
@@ -1,3 +1,11 @@
 (function() {
-  return document.title;
+  // https://github.com/sitespeedio/browsertime/issues/979
+  if (typeof document.title === "string") {
+    return document.title;
+  } else {
+    const titles = document.getElementsByTagName("title");
+    if (titles.length > 0) {
+      return titles[0].innerHTML;
+    } else return "";
+  }
 })();
