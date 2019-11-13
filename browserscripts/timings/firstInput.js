1d0ea7b67c550eca5f0b5d1c2ec78819ba091544
soulgalore <peter@soulgalore.com>
2019-11-13 21:15:07 +0100

    rename copy paste

--- browserscripts/timings/firstInput.js
+++ browserscripts/timings/firstInput.js
@@ -1,19 +1,19 @@
 (function() {
-    const supported = PerformanceObserver.supportedEntryTypes;
-    if (!supported || supported.indexOf('first-input') === -1) {
-      return;
-    }
-    const observer = new PerformanceObserver(list => {});
-    observer.observe({ type: 'first-input', buffered: true });
-    const entries = observer.takeRecords();
-    if (entries.length > 0) {
-      const largestEntry = entries[entries.length - 1];
-      return {
-        duration: largestEntry.duration,
-        name: largestEntry.name,
-        processingEnd: Number(largestEntry.processingEnd.toFixed(0)),
-        processingStart: Number(largestEntry.processingStart.toFixed(0)),
-        startTime: Number(largestEntry.startTime.toFixed(0)),
-      };
-    } else return;
-  })();
+  const supported = PerformanceObserver.supportedEntryTypes;
+  if (!supported || supported.indexOf("first-input") === -1) {
+    return;
+  }
+  const observer = new PerformanceObserver(list => {});
+  observer.observe({ type: "first-input", buffered: true });
+  const entries = observer.takeRecords();
+  if (entries.length > 0) {
+    const entry = entries[entries.length - 1];
+    return {
+      duration: entry.duration,
+      name: entry.name,
+      processingEnd: Number(entry.processingEnd.toFixed(0)),
+      processingStart: Number(entry.processingStart.toFixed(0)),
+      startTime: Number(entry.startTime.toFixed(0))
+    };
+  } else return;
+})();
