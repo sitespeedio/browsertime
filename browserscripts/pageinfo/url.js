d504ed4020c211d45d869906fa4f7506cb9b3b78
Peter Hedenskog <peter@soulgalore.com>
2019-11-17 08:41:04 -0500

    Be careful when getting the URL #979 (#992)

--- browserscripts/pageinfo/url.js
+++ browserscripts/pageinfo/url.js
@@ -1,3 +1,8 @@
 (function() {
+  // https://github.com/sitespeedio/browsertime/issues/979#issuecomment-549107350
+  if (typeof document.URL === "string") {
     return document.URL;
-  })();
+  } else {
+    return window.location.href;
+  }
+})();
