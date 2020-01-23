0d391cd086766e014326ed797f89312ea8a3d031
Tarek Ziade <tarek@ziade.org>
2020-01-23 08:53:10 +0100

    added a test on perceptual

--- browsertime/test_visualmetrics.py
+++ browsertime/test_visualmetrics.py
@@ -1,7 +1,10 @@
 import unittest
 import os
 
-from browsertime.visualmetrics import calculate_contentful_speed_index
+from browsertime.visualmetrics import (
+    calculate_contentful_speed_index,
+    calculate_perceptual_speed_index,
+)
 
 HERE = os.path.dirname(__file__)
 
@@ -18,4 +21,17 @@ class TestVisualMetrics(unittest.TestCase):
 
         progress = [_p(image) for image in images if image.startswith("ms_")]
         res = calculate_contentful_speed_index(progress, directory)
+        self.assertTrue(res[0], 5080)
+
+    def test_calculate_perceptual_speed_index(self):
+        directory = "test_data"
+        images = os.listdir(os.path.join(HERE, directory))
+
+        def _p(image):
+            p = {}
+            p["time"] = int(image.split(".")[0].split("ms_")[-1])
+            return p
+
+        progress = [_p(image) for image in images if image.startswith("ms_")]
+        res = calculate_perceptual_speed_index(progress, directory)
         self.assertTrue(res[0], 5080)
