sitespeedio/browsertime

commit edfc797d60c5760a2a9f6042045afaefc3fdd1a8

Simplify how we load a new URL and add retries. (#1077)
Sometimes Chrome on a test server can take a really long time to start
to navigate, that's why we are adding a retry functionaliry so if
we try three times to wait on the actual page to finish loading.

lib/core/seleniumRunner.js
@@ -191,11 +191,11 @@ class SeleniumRunner {
    */
   async loadAndWait(url, pageCompleteCheck) {
     const driver = this.driver;
-    try {
-      // To learn more about the event loop and request animation frame
-      // watch Jake Archibald on ‘The Event Loop’ https://vimeo.com/254947206
-      // TODO do we only want to do this when we record a video?
-      const navigate = `(function() {
+    const startURI = await driver.executeScript('return document.documentURI;');
+    // To learn more about the event loop and request animation frame
+    // watch Jake Archibald on ‘The Event Loop’ https://vimeo.com/254947206
+    // TODO do we only want to do this when we record a video?
+    const navigate = `(function() {
           const orange = document.getElementById('browsertime-orange');
           if (orange) {
             orange.parentNode.removeChild(orange);
@@ -206,29 +206,51 @@ class SeleniumRunner {
             });
           });
         })();`;
-      // Navigate to the page
-      await driver.executeScript(navigate);
-      // If you run with default settings, the webdriver will give back control after
-      // the onload event. But you can change that with the --pageLoadStrategy none
-      // Then you will be in control immediately and therefore wanna wait some extra time
-      // befor you start to run your page complete check
-      await delay(this.options.pageCompleteCheckStartWait || 500);
-      await this.wait(pageCompleteCheck);
-    } catch (e) {
-      log.error('Could not load URL' + e);
-      throw new UrlLoadError('Failed to load ' + url, url, {
-        cause: e
-      });
-    }
-    // E.g. when loading about:blank for the info page
-    if (url.match(/^http(s)?:\/\//i)) {
-      const uri = await driver.executeScript('return document.documentURI;');
-      // Verify that the page succesfully loaded
-      if (!uri.match(/^http(s)?:\/\//i)) {
-        throw new UrlLoadError(
-          'Failed to load/verify ' + url + ' uri:' + uri,
-          url
+    // Navigate to the page
+    await driver.executeScript(navigate);
+    // If you run with default settings, the webdriver will give back control after
+    // the onload event. But you can change that with the --pageLoadStrategy none
+    // Then you will be in control immediately and therefore wanna wait some extra time
+    // befor you start to run your page complete check
+    await delay(this.options.pageCompleteCheckStartWait || 500);
+
+    // We give it a couple of times to finish loading, this makes it
+    // more stable in real case scenarios on slow servers.
+    const tries = 3;
+    for (let i = 0; i < tries; ++i) {
+      try {
+        await this.wait(pageCompleteCheck);
+        const newURI = await driver.executeScript(
+          'return document.documentURI;'
         );
+        // If we use a SPA it could be that we don't test a new URL so just do one try
+        // and make sure your page complete check take care of other things
+        if (this.options.spa) {
+          break;
+        } else if (newURI !== startURI) {
+          // We navigated to a new page, we don't need to test anymore
+          break;
+        } else {
+          log.info(`URL failed to load, trying ${tries - i - 1} more time(s)`);
+          // We add some wait time before we try again
+          await delay(1000);
+        }
+      } catch (e) {
+        log.info(
+          `URL failed to load, trying ${tries - i - 1} more time(s): ${
+            e.message
+          }`
+        );
+        //
+        if (i === tries - 1) {
+          // If the last tries through an error, rethrow as before
+          log.error('Could not load URL %s', url, e);
+          throw new UrlLoadError('Failed to load ' + url, url, {
+            cause: e
+          });
+        } else {
+          await delay(1000);
+        }
       }
     }
   }

commit 9bbb9e9cc3fb22d29190932e5f73bff2611bd351

Compare "bare" domain names in URL retry logic as equal. (#1157)
This fixes an issue introduced in #1077: URL comparisons are failing
because the similar looking URLs for "bare" domain names like
https://x.com and https://x.com/ do not string-compare as ===.  It's
probably not worth fully normalizing URLs (i.e., sorting query
parameters) but it is worth normalizing trailing slashes in such
cases, which we do by parsing with `new URL(...)` (which is present
since Node v8).

lib/core/seleniumRunner.js
@@ -196,7 +196,12 @@ class SeleniumRunner {
    */
   async loadAndWait(url, pageCompleteCheck) {
     const driver = this.driver;
-    const startURI = await driver.executeScript('return document.documentURI;');
+    // Browsers may normalize 'https://x.com' differently; in particular, Firefox normalizes to
+    // 'https://x.com/'.  This is a first normalization attempt; there are deeper options that order
+    // query parameters, order fragments, etc.  We don't want to change url itself 'cuz it can be
+    // used for indexing collected data and it is possible that normalization could change a key.
+    const normalizedURI = new URL(url).toString();
+    const startURI = new URL(await driver.executeScript('return document.documentURI;')).toString();
     // To learn more about the event loop and request animation frame
     // watch Jake Archibald on ‘The Event Loop’ https://vimeo.com/254947206
     // TODO do we only want to do this when we record a video?
@@ -226,17 +231,16 @@ class SeleniumRunner {
     for (let i = 0; i < tries; ++i) {
       try {
         await this.wait(pageCompleteCheck);
-        const newURI = await driver.executeScript(
-          'return document.documentURI;'
-        );
+        const newURI = new URL(await driver.executeScript('return document.documentURI;')).toString();
+
         // If we use a SPA it could be that we don't test a new URL so just do one try
         // and make sure your page complete check take care of other things
         if (this.options.spa) {
           break;
-        } else if (url === startURI) {
+        } else if (normalizedURI === startURI) {
           // You are navigating to the current page
           break;
-        } else if (url.startsWith('data:text')) {
+        } else if (normalizedURI.startsWith('data:text')) {
           // Navigations between data/text seems to don't change the URI
           break;
         } else if (newURI !== startURI) {