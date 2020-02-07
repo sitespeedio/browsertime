af90f31aa0363a61352b5f7b8edf9609b2813764
Sean Feng <sefeng@mozilla.com>
2020-02-07 18:49:18 +0100

    Add new resource related performance metrics (#1167)
    These two numbers help us to compare resource differences
    between Browsers.

---
+++ browserscripts/pageinfo/resources.js
@@ -0,0 +1,14 @@
+(function() {
+  const resources = window.performance.getEntriesByType("resource");
+
+  let resourceDuration = 0;
+  for (let i = 0; i < resources.length; i++) {
+    resourceDuration += resources[i].duration;
+  }
+
+  return {
+    resourceCount: Number(resources.length),
+    resourceDuration: Number(resourceDuration)
+  }
+})();
+
