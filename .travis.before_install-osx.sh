3a611ea376769d7e1c6ba3f78799a1f36ba17c25
Peter Hedenskog <phedenskog@wikimedia.org>
2020-02-10 15:06:15 +0100

    Install file for OS X

---
+++ .travis.before_install-osx.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+brew update
+brew install ffmpeg imagemagick unzip wget
+pip install virtualenv 
+brew cask install microsoft-edge
+wget https://msedgedriver.azureedge.net/80.0.361.48/edgedriver_mac64.zip
+unzip edgedriver_mac64.zip
