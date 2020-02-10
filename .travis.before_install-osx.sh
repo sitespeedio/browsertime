20bd6ae69c61a7bf7bc86ee8fe7a6ca5a8ae0845
soulgalore <peter@soulgalore.com>
2020-02-11 00:05:51 +0100

    show devices

--- .travis.before_install-osx.sh
+++ .travis.before_install-osx.sh
@@ -8,3 +8,4 @@ wget https://msedgedriver.azureedge.net/79.0.309.65/edgedriver_mac64.zip
 unzip edgedriver_mac64.zip
 /Applications/Microsoft\ Edge.app/Contents/MacOS/Microsoft\ Edge --version
 sudo safaridriver --enable
+ffmpeg -hide_banner -f avfoundation -list_devices true -i ""
