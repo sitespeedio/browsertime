daa6bc44d6d3bf04a3ba8bbd6fddc1bc9c10c769
Peter Hedenskog <phedenskog@wikimedia.org>
2020-02-10 21:03:42 +0100

    try another msedgedriver

--- .travis.before_install-osx.sh
+++ .travis.before_install-osx.sh
@@ -4,7 +4,7 @@ brew update
 brew install ffmpeg imagemagick unzip
 pip install virtualenv 
 brew cask install microsoft-edge
-wget https://msedgedriver.azureedge.net/80.0.361.48/edgedriver_mac64.zip
+wget https://msedgedriver.azureedge.net/79.0.309.65/edgedriver_mac64.zip
 unzip edgedriver_mac64.zip
 /Applications/Microsoft\ Edge.app/Contents/MacOS/Microsoft\ Edge --version
 sudo safaridriver --enable
