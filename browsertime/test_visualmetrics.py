f4155c8f859db51db6b790dfa4832400d85bde3e
Sean Feng <sefeng@mozilla.com>
2020-02-10 14:19:12 -0500

    Fix CSI to use the correct number for calculation

--- browsertime/test_visualmetrics.py
+++ browsertime/test_visualmetrics.py
@@ -10,9 +10,9 @@ HERE = os.path.dirname(__file__)
 
 
 class TestVisualMetrics(unittest.TestCase):
-    def test_calculate_contentful_speed_index(self):
-        directory = "test_data"
-        images = os.listdir(os.path.join(HERE, directory))
+    def setUp(self):
+        self.directory = "test_data"
+        images = os.listdir(os.path.join(HERE, self.directory))
 
         def _p(image):
             p = {}
@@ -20,22 +20,15 @@ class TestVisualMetrics(unittest.TestCase):
             return p
 
         progress = [_p(image) for image in images if image.startswith("ms_")]
-        res = calculate_contentful_speed_index(sorted(progress,
-                                                      key = lambda image: image['time']),
-                                               directory)
-        self.assertEqual(res[0], 5080)
+        self.sorted_progress = sorted(progress,
+                                      key = lambda image: image['time'])
+
+    def test_calculate_contentful_speed_index(self):
+        res = calculate_contentful_speed_index(self.sorted_progress,
+                                               self.directory)
+        self.assertEqual(res[0], 1188)
 
     def test_calculate_perceptual_speed_index(self):
-        directory = "test_data"
-        images = os.listdir(os.path.join(HERE, directory))
-
-        def _p(image):
-            p = {}
-            p["time"] = int(image.split(".")[0].split("ms_")[-1])
-            return p
-
-        progress = [_p(image) for image in images if image.startswith("ms_")]
-        res = calculate_perceptual_speed_index(sorted(progress,
-                                                      key = lambda image: image['time']),
-                                               directory)
+        res = calculate_perceptual_speed_index(self.sorted_progress,
+                                               self.directory)
         self.assertEqual(res[0], 946)
