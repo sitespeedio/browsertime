6f5be4f3a1c81367fc5bd79eec7338ebec032f62
Tarek Ziade <tarek@ziade.org>
2020-03-09 17:19:37 +0100

    minimal patch for #1216

--- bin/browsertime.js
+++ bin/browsertime.js
@@ -10,6 +10,8 @@ const merge = require('lodash.merge');
 const fs = require('fs');
 const path = require('path');
 const log = require('intel').getLogger('browsertime');
+const engineUtils = require('../lib/support/engineUtils');
+
 
 async function parseUserScripts(scripts) {
   if (!Array.isArray(scripts)) scripts = [scripts];
@@ -49,7 +51,11 @@ async function run(urls, options) {
       let saveOperations = [];
 
       // TODO setup by name
-      const storageManager = new StorageManager(urls[0], options);
+      var first_url = urls[0];
+      if (first_url instanceof Array) {
+        first_url = first_url[0];
+      }
+      const storageManager = new StorageManager(first_url, options);
       const harName = options.har ? options.har : 'browsertime';
       const jsonName = options.output ? options.output : 'browsertime';
 
@@ -93,7 +99,32 @@ async function run(urls, options) {
 }
 
 let cliResult = cli.parseCommandLine();
+var tests = [];
+
+cliResult.urls.forEach(function convert(url) {
+   var testScript = engineUtils.loadScript(url);
+   // if the value is an url or a not an array we can return the original value
+   if (typeof testScript == "string" || !testScript instanceof Array) {
+    tests.push(url);
+    return;
+  }
+  if (testScript.setUp) {
+    if (!cliResult.options.preScript) {
+      cliResult.options.preScript = [];
+    }
+    cliResult.options.preScript.push(testScript.setUp);
+  }
+  if (testScript.tearDown) {
+    if (!cliResult.options.postScript) {
+      cliResult.options.postScript = [];
+    }
+    cliResult.options.postScript.push(testScript.tearDown);
+  }
+  testScript.tests.forEach(function convertTest(test) {
+    tests.push([url, test]);
+  });
+});
 
 logging.configure(cliResult.options);
 
-run(cliResult.urls, cliResult.options);
+run(tests, cliResult.options);
