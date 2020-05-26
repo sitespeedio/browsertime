894deb09e41751528029b52ad15da74992b1b645
soulgalore <peter@soulgalore.com>
2020-05-26 22:47:43 +0200

    correct network rights

--- test/test.sh
+++ test/test.sh
@@ -8,8 +8,8 @@ sudo modprobe ifb numifbs=1
 while true
 do
     git pull
-    docker run --rm -v "$(pwd)":/browsertime sitespeedio/browsertime-autobuild https://www.sitespeed.io/ --cpu --connectivity.engine throttle -c cable
-    docker run --shm-size 2g --rm -v "$(pwd)":/browsertime sitespeedio/browsertime-autobuild https://www.sitespeed.io/ -b firefox --firefox.geckoProfiler --connectivity.engine throttle -c cable
+    docker run --cap-add=NET_ADMIN --rm -v "$(pwd)":/browsertime sitespeedio/browsertime-autobuild https://www.sitespeed.io/ --cpu --connectivity.engine throttle -c cable
+    docker run --cap-add=NET_ADMIN --shm-size 2g --rm -v "$(pwd)":/browsertime sitespeedio/browsertime-autobuild https://www.sitespeed.io/ -b firefox --firefox.geckoProfiler --connectivity.engine throttle -c cable
     rm -fR browsertime-results
     delay 120 
 done
