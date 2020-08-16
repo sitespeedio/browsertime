sitespeedio/browsertime

commit 206afb39332c2b0d10ed209568b610b9784ced0d

Remove duplicate HAR code due to merge and fix wrong progress (#1042)

lib/support/har/index.js
@@ -35,7 +35,7 @@ function addTimingsToHAR(harPage, visualMetricsData, timings, cpu) {
       'ContentfulSpeedIndex',
       'VisualProgress',
       'ContentfulSpeedIndexProgress',
-      'PerceptualSpeedIndexProgres'
+      'PerceptualSpeedIndexProgress'
     ];
 
     for (let key of Object.keys(visualMetricsData)) {
@@ -43,7 +43,7 @@ function addTimingsToHAR(harPage, visualMetricsData, timings, cpu) {
         harPageTimings['_' + key.charAt(0).toLowerCase() + key.slice(1)] =
           visualMetricsData[key];
         _visualMetrics[key] = visualMetricsData[key];
-      } else if (key !== 'VisualProgress') {
+      } else if (key.indexOf('Progress') === 0) {
         _visualMetrics[key] = visualMetricsData[key];
       }
     }
@@ -202,74 +202,6 @@ module.exports = {
     return combinedHar;
   },
 
-  addMetaToHAR(index, harPage, url, options) {
-    const _meta = (harPage._meta = {});
-    _meta.connectivity = get(options, 'connectivity.profile', 'native');
-    _meta.connectivity = get(options, 'connectivity.alias', _meta.connectivity);
-
-    if (options.resultURL) {
-      const base = options.resultURL.endsWith('/')
-        ? options.resultURL
-        : options.resultURL + '/';
-      if (options.screenshot) {
-        _meta.screenshot = `${base}${pathToFolder(
-          url,
-          options
-        )}screenshots/${index + 1}.${options.screenshotParams.type}`;
-      }
-      if (options.video) {
-        _meta.video = `${base}${pathToFolder(url, options)}video/${index +
-          1}.mp4`;
-      }
-      if (options.chrome && options.chrome.timeline) {
-        _meta.timeline = `${base}${pathToFolder(url, options)}trace-${index +
-          1}.json.gz`;
-      }
-    }
-  },
-
-  addTimingsToHAR(harPage, visualMetricsData, timings, cpu) {
-    const harPageTimings = harPage.pageTimings;
-
-    const _visualMetrics = (harPage._visualMetrics = {});
-    harPage._cpu = cpu;
-
-    // We add the timings both as a hidden field and add
-    // in pageTimings so we can easily show them in PerfCascade
-    if (visualMetricsData) {
-      const DO_NOT_INCLUDE_IN_HAR_TIMINGS = [
-        'VisualReadiness',
-        'SpeedIndex',
-        'PerceptualSpeedIndex',
-        'ContentfulSpeedIndex',
-        'VisualProgress'
-      ];
-
-      for (let key of Object.keys(visualMetricsData)) {
-        if (DO_NOT_INCLUDE_IN_HAR_TIMINGS.indexOf(key) === -1) {
-          harPageTimings['_' + key.charAt(0).toLowerCase() + key.slice(1)] =
-            visualMetricsData[key];
-          _visualMetrics[key] = visualMetricsData[key];
-        } else if (key !== 'VisualProgress') {
-          _visualMetrics[key] = visualMetricsData[key];
-        }
-      }
-
-      // Convert to sitespeedio's compare visual progress format.
-      _visualMetrics.VisualProgress = jsonifyVisualProgress(
-        visualMetricsData.VisualProgress
-      );
-    } else if (timings && timings.firstPaint) {
-      // only add first paint if we don't have visual metrics
-      harPageTimings._firstPaint = timings.firstPaint;
-    }
-    if (timings && timings.pageTimings) {
-      harPageTimings._domInteractiveTime =
-        timings.pageTimings.domInteractiveTime;
-      harPageTimings._domContentLoadedTime =
-        timings.pageTimings.domContentLoadedTime;
-    }
-  },
   getEmptyHAR(url, browser) {
     return {
       log: {

commit fa037b01622737c7e4e728e6ccd8e22ddd2df740

Fix missing SpeedIndex in HAR
Commit 206afb39332c2b0d10ed209568b610b9784ced0d introduced a bug where
the SpeedIndex and PerceptualSpeedIndex metrics are no longer added to
the HAR under "har.log.pages[]._visualMetrics". This is causing problems
with Sitespeed.io Compare, since it now can't find the SpeedIndex.

I'm not 100% sure why that commit changed the condition to
"key.indexOf('Progress') === 0)", since there aren't any metrics that
start with "Progress". I'm guessing the intent was to check if the
metric name ends with "Progress", so I changed it do that. I also added
tests.

lib/support/har/index.js
@@ -43,7 +43,7 @@ function addTimingsToHAR(harPage, visualMetricsData, timings, cpu) {
         harPageTimings['_' + key.charAt(0).toLowerCase() + key.slice(1)] =
           visualMetricsData[key];
         _visualMetrics[key] = visualMetricsData[key];
-      } else if (key.indexOf('Progress') === 0) {
+      } else if (!key.endsWith('Progress')) {
         _visualMetrics[key] = visualMetricsData[key];
       }
     }

test/unitTests/harBuilderTests.js
@@ -114,4 +114,107 @@ describe('har_builder', function() {
       );
     });
   });
+
+  describe('#addExtraFieldsToHar', function() {
+    let options;
+    let totalResults;
+
+    beforeEach(function() {
+      options = {
+        iterations: 1
+      };
+      totalResults = [
+        {
+          visualMetrics: [
+            {
+              SpeedIndex: 100,
+              PerceptualSpeedIndex: 200,
+              ContentfulSpeedIndex: 300,
+              VisualProgress: [
+                { timestamp: 0, percent: 0 },
+                { timestamp: 10, percent: 50 },
+                { timestamp: 20, percent: 100 }
+              ]
+            }
+          ],
+          info: {
+            url: 'https://example.com'
+          },
+          cpu: [
+            {
+              longTasks: {
+                tasks: 1,
+                totalBlockingTime: 10
+              }
+            }
+          ],
+          browserScripts: [
+            {
+              timings: {
+                firstPaint: 100,
+                largestContentfulPaint: {
+                  renderTime: 200
+                }
+              }
+            }
+          ]
+        }
+      ];
+    });
+
+    it('should gracefully handle missing data', function() {
+      builder.addExtraFieldsToHar();
+    });
+
+    it('should add visual metrics if given', function() {
+      har.log.pages[0].pageTimings = {};
+      builder.addExtraFieldsToHar(totalResults, har, options);
+
+      expect(har.log.pages[0]).to.eql({
+        id: 'page_0',
+        _meta: {
+          connectivity: 'native'
+        },
+        _visualMetrics: {
+          SpeedIndex: 100,
+          PerceptualSpeedIndex: 200,
+          ContentfulSpeedIndex: 300,
+          VisualProgress: {
+            '0': 0,
+            '10': 50,
+            '20': 100
+          }
+        },
+        pageTimings: {
+          _largestContentfulPaint: 200
+        },
+        _cpu: {
+          longTasks: {
+            tasks: 1,
+            totalBlockingTime: 10
+          }
+        }
+      });
+    });
+
+    it('should gracefully handle missing cpu and visual metrics', function() {
+      har.log.pages[0].pageTimings = {};
+      totalResults[0].visualMetrics = [];
+      totalResults[0].cpu = [{}];
+      builder.addExtraFieldsToHar(totalResults, har, options);
+
+      expect(har.log.pages[0]).to.eql({
+        id: 'page_0',
+        _meta: {
+          connectivity: 'native'
+        },
+        _cpu: {},
+        _visualMetrics: {},
+        pageTimings: {
+          _firstPaint: 100,
+          _largestContentfulPaint: 200
+        }
+      });
+    });
+  });
 });