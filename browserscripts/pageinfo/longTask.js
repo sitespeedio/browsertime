5bbb814c8ba35403538b47cf61b56eb6d5251545
Peter Hedenskog <peter@soulgalore.com>
2020-08-30 20:32:48 +0200

    Cleanup Long Tasks for Chrome (#1341)
    A couple of releases ago, Chrome started to support buffered long tasks,
    that means we don't need to inject any JS to make sure we catch all long tasks.
    
    The code is simpler and since (hopefully) the new buffered version doesn't
    give any performance penelty, we can start getting long	tasks by default. That
    means this needs to go in the next major version

--- browserscripts/pageinfo/longTask.js
+++ browserscripts/pageinfo/longTask.js
@@ -1,25 +1,28 @@
 (function(minLength) {
-  if (window.__bt_longtask) {
-    const cleaned = [];
-    for (let event of window.__bt_longtask.e) {
-      if (event.duration >= minLength) {
-        const e = {};
-        e.duration = event.duration;
-        e.name = event.name;
-        e.startTime = event.startTime;
-        e.attribution = [];
-        for (let at of event.attribution) {
-          const a = {};
-          a.containerId = at.containerId;
-          a.containerName = at.containerName;
-          a.containerSrc = at.containerSrc;
-          a.containerType = at.containerType;
-          e.attribution.push(a);
-        }
-        cleaned.push(e);
-      }
-    }
-    window.__bt_longtask.e = [];
-    return cleaned;
+  const supported = PerformanceObserver.supportedEntryTypes;
+  if (!supported || supported.indexOf('longtask') === -1) {
+    return;
   }
+  const longTaskObserver = new PerformanceObserver(list => {});
+  longTaskObserver.observe({type: 'longtask', buffered: true});
+  const cleaned = [];
+  for (let entry of longTaskObserver.takeRecords()) {
+    if (entry.duration >= minLength) {
+      const e = {};
+      e.duration = entry.duration;
+      e.name = entry.name;
+      e.startTime = entry.startTime;
+      e.attribution = [];
+      for (let at of entry.attribution) {
+        const a = {};
+        a.containerId = at.containerId;
+        a.containerName = at.containerName;
+        a.containerSrc = at.containerSrc;
+        a.containerType = at.containerType;
+        e.attribution.push(a);
+      }
+      cleaned.push(e);
+    } 
+  }
+  return cleaned;  
 })(arguments[arguments.length - 1]);
