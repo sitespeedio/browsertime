04574688f3c16c2c712cc85e11af8634d959d874
Peter Hedenskog <peter@soulgalore.com>
2020-10-26 21:23:22 +0100

    Navigate to the same URL should generate multiple tests. (#1421)
    * Navigate to the same URL should generate multiple tests.
    
    In 10 testing the same page twice in a script was broken as reported
    in #1419 and also reported in https://github.com/sitespeedio/sitespeed.io/issues/3164.
    
    The problem was that when we stop the video, we didn't know the correct URL,
    that is fixed with this commit (+code cleanup to make it easier to follow).
    
    * Add test for navigating to the same page

---
+++ test/navigationscript/sameURLTwice.js
@@ -0,0 +1,5 @@
+module.exports = async function(context, commands) {
+  await commands.measure.start('https://www.sitespeed.io');
+  await commands.navigate('about:blank');
+  return commands.measure.start('https://www.sitespeed.io');
+};
