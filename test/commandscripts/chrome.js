df91c8bd249ce5a686761e577dac709b0ced0096
Peter Hedenskog <peter@soulgalore.com>
2021-01-23 16:08:52 +0100

    Make it possible to listen to CDP events in scripting (#1473)
    * Make it possible to listen to CDP events in scripting
    
    * Add test

--- test/commandscripts/chrome.js
+++ test/commandscripts/chrome.js
@@ -1,4 +1,8 @@
 module.exports = async function(context, commands) {
+  const responses = [];
+  await commands.cdp.on('Network.responseReceived', params => {
+    responses.push(params);
+  });
   await commands.measure.start('https://www.sitespeed.io/search/');
   await commands.cdp.send('Network.clearBrowserCookies');
   await commands.cdp.sendAndGet('Memory.getDOMCounters');
