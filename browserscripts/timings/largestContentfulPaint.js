c5f94d8d4abfb1ba01fd9caad4b5a78b61cbeff2
Peter Hedenskog <peter@soulgalore.com>
2021-04-30 05:26:58 +0200

    More and more useful info about LCP (#1561)
    - collect all LCP 
    - get the DOM path
    - store a screenshot with the LCP marked

--- browserscripts/timings/largestContentfulPaint.js
+++ browserscripts/timings/largestContentfulPaint.js
@@ -1,4 +1,32 @@
 (function() {
+  // https://gist.github.com/karlgroves/7544592
+  function getDomPath(el) {
+    const stack = [];
+    while ( el.parentNode != null ) {
+      let sibCount = 0;
+      let sibIndex = 0;
+      for ( let i = 0; i < el.parentNode.childNodes.length; i++ ) {
+        let sib = el.parentNode.childNodes[i];
+        if ( sib.nodeName == el.nodeName ) {
+          if ( sib === el ) {
+            sibIndex = sibCount;
+          }
+          sibCount++;
+        }
+      }
+      if ( el.hasAttribute && el.hasAttribute('id') && el.id != '' ) {
+        stack.unshift(el.nodeName.toLowerCase() + '#' + el.id);
+      } else if ( sibCount > 1 ) {
+        stack.unshift(el.nodeName.toLowerCase() + ':eq(' + sibIndex + ')');
+      } else {
+        stack.unshift(el.nodeName.toLowerCase());
+      }
+      el = el.parentNode;
+    }
+  
+    return stack.slice(1);
+  }
+
   const supported = PerformanceObserver.supportedEntryTypes;
   if (!supported || supported.indexOf('largest-contentful-paint') === -1) {
     return;
@@ -16,7 +44,9 @@
       renderTime: Number(Math.max(largestEntry.renderTime,largestEntry.loadTime).toFixed(0)),
       size: largestEntry.size,
       startTime: Number(largestEntry.startTime.toFixed(0)),
-      tagName: largestEntry.element ? largestEntry.element.tagName : ''
+      tagName: largestEntry.element ? largestEntry.element.tagName : '',
+      className :largestEntry.element ? largestEntry.element.className : '',
+      domPath:  largestEntry.element ? (getDomPath(largestEntry.element)).join( ' > ') : ''
     };
   } else return;
 })();
