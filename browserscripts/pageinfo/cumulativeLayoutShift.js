bb40db24661ca5466a0d962fce78729246916d33
Peter Hedenskog <peter@soulgalore.com>
2021-06-03 17:42:17 +0200

    Use the new defintion of Cummulative Layout Shift (#1591)

--- browserscripts/pageinfo/cumulativeLayoutShift.js
+++ browserscripts/pageinfo/cumulativeLayoutShift.js
@@ -4,15 +4,25 @@
     return;
   }
   // See https://web.dev/layout-instability-api
+  // https://github.com/mmocny/web-vitals/wiki/Snippets-for-LSN-using-PerformanceObserver#max-session-gap1s-limit5s
+  let max = 0;
+  let curr = 0; 
+  let firstTs = Number.NEGATIVE_INFINITY;
+  let  prevTs = Number.NEGATIVE_INFINITY;
   const observer = new PerformanceObserver(list => {});
   observer.observe({ type: 'layout-shift', buffered: true });
   const list = observer.takeRecords();
-  let score = 0;
   for (let entry of list) {
     if (entry.hadRecentInput) {
       continue;
     } 
-    score += entry.value;
+    if (entry.startTime - firstTs > 5000 || entry.startTime - prevTs > 1000) {
+      firstTs = entry.startTime;
+      curr = 0;
+    }
+    prevTs = entry.startTime;
+    curr += entry.value;
+    max = Math.max(max, curr);
   }
-  return score;
+  return max;
 })();
--- browserscripts/pageinfo/cumulativeLayoutShiftInfo.js
+++ browserscripts/pageinfo/cumulativeLayoutShiftInfo.js
@@ -33,14 +33,29 @@
     }
     // See https://web.dev/layout-instability-api
     const observer = new PerformanceObserver(list => {});
-    const entries = [];
+    let max = 0; 
+    let current = []; 
+    let curr = 0; 
+    let firstTs = Number.NEGATIVE_INFINITY; 
+    let prevTs = Number.NEGATIVE_INFINITY;
     observer.observe({ type: 'layout-shift', buffered: true });
     const list = observer.takeRecords();
-    let score = 0;
+    const maxSessionGap1sLimit5s = {};
     for (let entry of list) {
       if (entry.hadRecentInput) {
         continue;
       } 
+
+      if (entry.startTime - firstTs > 5000 || entry.startTime - prevTs > 1000) {
+        firstTs = entry.startTime;
+        curr = 0;
+        maxSessionGap1sLimit5s[max] = [...current];
+        current = []; 
+      }
+      prevTs = entry.startTime;
+      curr += entry.value;
+      max = Math.max(max, curr);
+
       const scoreAndHTML = {score: entry.value, domPath: [], startTime: entry.startTime, tags: []};
       for (let source of entry.sources) {
         try {
@@ -53,8 +68,8 @@
         }
         catch(e) {}
       }
-     entries.push(scoreAndHTML);
-    }
-    return entries;
+      current.push(scoreAndHTML);
+      }
+    return maxSessionGap1sLimit5s[max] ?  maxSessionGap1sLimit5s[max] : current;
   })();
   
