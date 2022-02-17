33c416c975f5484b840c43de542bd71e0bb91acb
soulgalore <peter@soulgalore.com>
2022-02-17 16:55:15 +0100

    Run Firefox appconstants code only for Firefox (and do not run async)

--- browserscripts/browser/appConstants.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  requires: { privilege: true },
-  function: function() {
-    const { AppConstants } = ChromeUtils.import(
-      'resource://gre/modules/AppConstants.jsm'
-    );
-    return AppConstants;
-  }
-};
