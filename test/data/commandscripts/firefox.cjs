d3ad3c5b5e1896d365432d0df97fd3d12d30d7e5
soulgalore <peter@soulgalore.com>
2023-04-13 17:55:40 +0200

    fix

--- test/data/commandscripts/firefox.cjs
+++ test/data/commandscripts/firefox.cjs
@@ -1,6 +1,6 @@
 module.exports = async function (context, commands) {
 
-  await commands.profile.start();
+  await commands.profiler.start();
   await commands.measure.start('http://127.0.0.1:3000/simple/');
-  return commands.profile.stop();
+  return commands.profiler.stop();
 };
