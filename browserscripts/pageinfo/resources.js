e3e5a46f6c9070c0ab66e543b85e60d021edc848
Peter Hedenskog <peter@soulgalore.com>
2023-10-12 16:32:55 +0200

    Collect number of assets served from browser cache (#1998)

--- browserscripts/pageinfo/resources.js
+++ browserscripts/pageinfo/resources.js
@@ -1,14 +1,27 @@
-(function() {
+(function () {
   const resources = window.performance.getEntriesByType('resource');
 
   let resourceDuration = 0;
+  let servedFromCache = 0;
+  let servedFromCacheSupported = false;
   for (let i = 0; i < resources.length; i++) {
     resourceDuration += resources[i].duration;
+    if (resources[i].deliveryType !== undefined) {
+      servedFromCacheSupported = true;
+      if (resources[i].deliveryType === 'cache') {
+        servedFromCache++;
+      }
+    }
   }
 
-  return {
+  const info = {
     count: Number(resources.length),
-    duration: Number(resourceDuration)
-  }
-})();
+    duration: Number(resourceDuration),
+  };
 
+  if (servedFromCacheSupported === true) {   
+    info.servedFromCache = Number(servedFromCache);
+  }
+
+  return info;
+})();
