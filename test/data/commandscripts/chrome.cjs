b778fd78ca0351ccc78a513da0e33f2bf4333db4
Peter Hedenskog <peter@soulgalore.com>
2023-12-11 04:47:28 +0100

    Update to ava 6 (#2028)
    * Update to ava 6
    
    * Fix broken Chrome trace tests

--- test/data/commandscripts/chrome.cjs
+++ test/data/commandscripts/chrome.cjs
@@ -5,10 +5,9 @@ module.exports = async function (context, commands) {
   });
   await commands.measure.start('http://127.0.0.1:3000/simple/');
   await commands.cdp.send('Network.clearBrowserCookies');
-
-  await commands.chromeTrace.start();
+  await commands.trace.start();
   await commands.measure.start('http://127.0.0.1:3000/dimple/');
-  await commands.chromeTrace.stop();
+  await commands.trace.stop();
 
   return commands.cdp.sendAndGet('Memory.getDOMCounters');
 };
