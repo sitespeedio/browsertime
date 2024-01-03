0562bd753916d9de5ab9f0c802210b01d56ab67a
soulgalore <peter@soulgalore.com>
2024-01-03 09:58:30 +0100

    docs: better cache docs

--- types/core/engine/command/cache.d.ts
+++ types/core/engine/command/cache.d.ts
@@ -1,16 +1,44 @@
 export class Cache {
     constructor(browser: any, browserName: any, extensionServer: any, cdp: any);
-    browser: any;
-    browserName: any;
-    extensionServer: any;
-    cdp: any;
     /**
-     * Clear the browser cache. Will clear browser cache and cookies.
+     * @private
      */
-    clear(): Promise<any>;
+    private browser;
     /**
-     * Clear the browser cache but keep cookies.
+     * @private
      */
-    clearKeepCookies(): Promise<any>;
+    private browserName;
+    /**
+     * @private
+     */
+    private extensionServer;
+    /**
+     * @private
+     */
+    private cdp;
+    /**
+     * Clears the browser cache. This includes both cache and cookies.
+     *
+     * For Firefox, it uses the extensionServer setup with specific options.
+     * For Chrome and Edge, it uses the Chrome DevTools Protocol (CDP) commands.
+     * If the browser is not supported, logs an error message.
+     *
+     * @async
+     * @throws Will throw an error if the browser is not supported.
+     * @returns {Promise<void>} A promise that resolves when the cache and cookies are cleared.
+     */
+    clear(): Promise<void>;
+    /**
+     * Clears the browser cache while keeping the cookies.
+     *
+     * For Firefox, it uses the extensionServer setup with specific options.
+     * For Chrome and Edge, it uses the Chrome DevTools Protocol (CDP) command to clear the cache.
+     * If the browser is not supported, logs an error message.
+     *
+     * @async
+     * @throws Will throw an error if the browser is not supported.
+     * @returns {Promise<void>} A promise that resolves when the cache is cleared but cookies are kept.
+     */
+    clearKeepCookies(): Promise<void>;
 }
 //# sourceMappingURL=cache.d.ts.map
