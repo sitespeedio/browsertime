7b2e4e48b924e5103ef005502df12209ce6b8448
soulgalore <peter@soulgalore.com>
2024-01-06 11:35:58 +0100

    Simple action test

---
+++ test/data/commandscripts/actions.cjs
@@ -0,0 +1,13 @@
+module.exports = async function (context, commands) {
+
+  await commands.measure.start('https://selenium.dev/selenium/web/mouse_interaction.html');
+  const clickable = await commands.element.byId('clickable');
+  return commands.action.getActions()
+        .move({ origin: clickable })
+        .pause(1000)
+        .press()
+        .pause(1000)
+        .sendKeys('abc')
+        .perform();
+ 
+};
