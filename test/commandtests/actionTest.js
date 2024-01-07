d1a4b872be82841cc6a7773c33a3a1ac1d6f8802
Peter Hedenskog <peter@soulgalore.com>
2024-01-07 12:29:01 +0100

    Add access to Selenium Action API. (#2061)
    * Add access to Selenium Action API.
    
    The Action API gives us the ability to write so much cleaner code.

---
+++ test/commandtests/actionTest.js
@@ -0,0 +1,40 @@
+import test from 'ava';
+const { before, after, serial } = test;
+import { resolve } from 'node:path';
+import { getEngine } from '../util/engine.js';
+import { startServer, stopServer } from '../util/httpserver.js';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const timeout = 20_000;
+
+let engine;
+
+function getPath(file) {
+  return resolve(__dirname, '..', 'data', 'commandscripts', file);
+}
+
+before('Setup the HTTP server', () => {
+  return startServer();
+});
+
+after.always('Stop the HTTP server', () => {
+  return stopServer();
+});
+
+serial.beforeEach('Start the browser', async t => {
+  t.timeout(timeout);
+  engine = getEngine();
+  return engine.start();
+});
+
+serial('Run through the action API', async t => {
+  const result = await engine.runMultiple([getPath('actions.cjs')], {
+    scripts: { uri: 'document.documentURI' }
+  });
+  t.deepEqual(
+    result[0].browserScripts[0].scripts.uri,
+    'http://127.0.0.1:3000/simple/'
+  );
+});
