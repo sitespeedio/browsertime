94b0bc1e2a994f45e64b3c519f6a501ebf6fed2c
soulgalore <peter@soulgalore.com>
2024-03-11 08:05:12 +0100

    Expose Bidi in commands

--- test/data/commandscripts/firefox.cjs
+++ test/data/commandscripts/firefox.cjs
@@ -1,5 +1,14 @@
 module.exports = async function (context, commands) {
 
+  const params = {
+    method: 'script.addPreloadScript',
+    params: {
+      functionDeclaration: 'function(){alert("hepp");}'
+    }
+  };
+
+  await commands.bidi.send(params);
+
   await commands.profiler.start();
   await commands.measure.start('http://127.0.0.1:3000/simple/');
   return commands.profiler.stop();
