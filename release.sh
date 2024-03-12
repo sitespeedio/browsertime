0b04036483dafb554f55fdd0008892e4440f3ead
soulgalore <peter@soulgalore.com>
2024-03-12 07:38:15 +0100

    A better release script

--- release.sh
+++ release.sh
@@ -8,8 +8,14 @@ set -e
 # Remove the node modules and the result dir to start clean
 rm -fR browsertime-results
 
+# Genereate types
+npm run tsc
+
 np $* --no-yarn
 
 bin/browsertime.js --help > ../sitespeed.io/docs/documentation/browsertime/configuration/config.md
 
 bin/browsertime.js --version | tr -d '\n' > ../sitespeed.io/docs/_includes/version/browsertime.txt
+
+# Generate documentation
+npm run jsdoc
