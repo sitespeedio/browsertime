9365a5907e7de50dbb51b81338fc2412a6a6f81c
Peter Hedenskog <peter@soulgalore.com>
2025-03-11 14:42:45 +0100

    Get server timing from main response instead of next resource (#2268)

--- browserscripts/timings/serverTimings.js
+++ browserscripts/timings/serverTimings.js
@@ -1,16 +1,12 @@
 (function () {
-    // https://developer.mozilla.org/en-US/docs/Web/API/PerformanceServerTiming
-    const entries = window.performance.getEntriesByType('resource');
-    if (entries.length > 0 && entries[0].serverTiming) {
-        const timings = entries[0].serverTiming;
-        const serverTimings = [];
-        for (let timing of timings) {
-             serverTimings.push({
-                 name: timing.name,
-                 duration: timing.duration,
-                 description: timing.description
-             });
-        }
-        return serverTimings;
-    } else return undefined;
+    let t = window.performance.getEntriesByType('navigation')[0];
+    const serverTimings = [];
+    for (let timing of t.serverTiming) {
+        serverTimings.push({
+            name: timing.name,
+            duration: timing.duration,
+            description: timing.description
+        });
+    }
+    return serverTimings;
 })();
