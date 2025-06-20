bb38169756f40774d1f8373795c983d03b103805
Gregory Mierzwinski <gmierz1@live.ca>
2025-06-20 15:15:46 -0400

    Check if serverTiming is defined before gathering its metrics. (#2301)

--- browserscripts/timings/serverTimings.js
+++ browserscripts/timings/serverTimings.js
@@ -1,12 +1,14 @@
 (function () {
     let t = window.performance.getEntriesByType('navigation')[0];
     const serverTimings = [];
-    for (let timing of t.serverTiming) {
-        serverTimings.push({
-            name: timing.name,
-            duration: timing.duration,
-            description: timing.description
-        });
+    if (t.serverTiming) {
+        for (let timing of t.serverTiming) {
+            serverTimings.push({
+                name: timing.name,
+                duration: timing.duration,
+                description: timing.description
+            });
+        }
     }
     return serverTimings;
 })();
