da1388d7011fc1d5b9b30f00f4fe129d9d295ebb
Abhishek Nimalan <abhisheknimalan@gmail.com>
2025-07-20 22:03:50 -0400

    Add simpleperf and perfetto support

---
+++ types/core/engine/command/simpleperf.d.ts
@@ -0,0 +1,43 @@
+export class SimplePerfProfiler {
+    constructor(browser: any, index: any, storageManager: any, options: any);
+    /**
+     * @private
+     */
+    private browser;
+    /**
+     * @private
+     */
+    private storageManager;
+    /**
+     * @private
+     */
+    private options;
+    /**
+     * @private
+     */
+    private index;
+    /**
+     * @private
+     */
+    private running;
+    /**
+     * Start Simpleperf profiling.
+     *
+     * @async
+     * @returns {Promise<void>} A promise that resolves when simpleperf has started profiling.
+     * @throws {Error} Throws an error if app_profiler.py fails to execute.
+     */
+    start(profilerOptions?: string, dirName?: string): Promise<void>;
+    dataDir: any;
+    simpleperfProcess: import("execa").ResultPromise<{}>;
+    /**
+     * Stop Simpleperf profiling.
+     *
+     * @async
+     * @returns {Promise<void>} A promise that resolves when simpleperf has stopped profiling
+     *                          and collected profile data.
+     * @throws {Error} Throws an error if app_profiler.py fails to execute.
+     */
+    stop(): Promise<void>;
+}
+//# sourceMappingURL=simpleperf.d.ts.map
